b6903eb39ea369a5bdd97d3710e6358f341159fd
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-13 21:30:16 +0300

    Operations between variables

--- javascript-1/script.js
+++ javascript-1/script.js
@@ -1,10 +1,16 @@
-let numar1 = 1;
-let numar2 = 1;
-
-// conventie: nu se atribuie alte tipuri de date fata de declaratie
-// numar1 = "1";
-// numar2 = "1";
+let numar1 = 100;
+let numar2 = 6;
 
 let suma = numar1 + numar2;
+let diferenta = numar1 - numar2;
+let produs = numar1 * numar2;
+let catulCuVirgula = numar1 / numar2;
+let restul = numar1 % numar2;
+let catul = Math.floor(numar1 / numar2);
 
 console.log(suma);
+console.log(diferenta);
+console.log(produs);
+console.log(catulCuVirgula);
+console.log(restul);
+console.log(catul);
