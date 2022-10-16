dcb38f74f2d8c920aa618d17f610ef2849c3d3bd
Groza Zita <adi_groza@yahoo.com>
2022-10-16 22:16:27 +0300

    Created Exercitiul 4

---
+++ tema1/script3.js
@@ -0,0 +1,29 @@
+console.log("EXERCITIUL 3");
+
+var a = "Luni";
+Luni = 0;
+console.log(`${a}: ${Luni}`);
+
+var a = "Marti";
+Marti = Luni + 1;
+console.log(`${a}: ${Marti}`);
+
+var a = "Miercuri";
+Miercuri = Luni + 2;
+console.log(`${a}: ${Miercuri}`);
+
+var a = "Joi";
+Joi = Luni + 3;
+console.log(`${a}: ${Joi}`);
+
+var a = "Vineri";
+Vineri = Luni + 4;
+console.log(`${a}: ${Vineri}`);
+
+var a = "Sambata";
+Sambata = Luni + 5;
+console.log(`${a}: ${Sambata}`);
+
+var a = "Duminica";
+Duminica = Luni + 6;
+console.log(`${a}: ${Duminica}`);
---
+++ tema1/script4.js
@@ -0,0 +1,8 @@
+console.log("EXERCITIUL 4");
+
+let a = "Nume";
+Nume = "Groza";
+let b = "Prenume";
+Prenume = "Adriana";
+
+console.log(`Numele meu este: ${Prenume} ${Nume}`);
