b6dc190bb1bf803d667f2e79a1746590206ad304
test.git <112196087+Andrei-drg@users.noreply.github.com>
2022-10-16 20:24:56 +0300

    223

---
+++ Exercitii/index.js
@@ -0,0 +1,15 @@
+
+console.log("EXERCITIUL 1")
+
+
+
+console.log(" 'a' a fost declarat")
+
+console.log('valoarea lui a este ${a}')
+
+let a ="10"
+
+console.log("lui a i s-a atribuit 10")
+
+console.log('valoarea lui a este ${a}')
+
