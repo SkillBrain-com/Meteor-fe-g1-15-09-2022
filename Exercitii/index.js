f552de187ae21d681ab5973ff98227076434906a
test.git <112196087+Andrei-drg@users.noreply.github.com>
2022-10-16 19:21:04 +0300

    second

--- Exercitii/index.js
+++ Exercitii/index.js
@@ -1,3 +1,15 @@
-let exercitiu = "EXERCITIUL 1"
 
 console.log("EXERCITIUL 1")
+
+
+
+console.log(" 'a' a fost declarat")
+
+console.log('valoarea lui a este ${a}')
+
+let a ="10"
+
+console.log("lui a i s-a atribuit 10")
+
+console.log('valoarea lui a este ${a}')
+
