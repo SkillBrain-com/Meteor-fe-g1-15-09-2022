fe9a6c3c796784a08aa1dd5a966e4dc3fefabad3
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-10-16 23:43:50 +0300

    schimbat valoarea lui a

--- Paul-tema1/script1.js
+++ Paul-tema1/script1.js
@@ -1,10 +1,14 @@
-console.log("EXERCITIUL 1");
+console.log('EXERCITIUL 1');
 
-let a;
+let a = 10;
 
-console.log("a a fost declarat");
-console.log("valoarea lui a este ${a}");
+console.log('a a fost declarat');
+console.log(`valoarea lui a este: ${a}`);
 
-a=10;
+console.log('lui a i s-a atribuit 10');
+console.log(`valoarea lui a este: ${a}`);
 
-console.log("lui a i s-a atribuit 10");
+ a = 20;
+
+console.log('a si-a schimbat valoarea');
+console.log(`Valoarea lui a este: ${a}`);
