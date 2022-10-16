1185a196a7d898090b9c63c298055cd6e1469457
Groza Zita <adi_groza@yahoo.com>
2022-10-16 22:02:57 +0300

    Created exercitiul 2

---
+++ tema1/script2.js
@@ -0,0 +1,27 @@
+console.log("EXERCITIUL 2");
+
+var variabila = "b";
+
+console.log("b" + " " + "a fost declarat");
+
+b = 10;
+
+console.log(`valoarea lui ${variabila} este ${variabila} = ${b}`);
+
+//console.log (`${variabila} = ${b}`);
+
+console.log(`lui ${variabila} i s-a atribuit ${b}`);
+
+console.log (`${variabila} = ${b}`);
+
+b = 20;
+
+console.log(`${variabila} si-a schimbat valoarea`);
+
+console.log (`${variabila} = ${b}`);
+
+b -= 5;
+
+console.log(`${variabila} si-a micsorat valoarea cu 5`);
+
+console.log (`valoarea lui ${variabila} este ${b}`);
