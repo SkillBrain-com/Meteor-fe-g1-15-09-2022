4e9eb9837b7fcf870fc692439f7bba6f26dd4bc2
test.git <112196087+Andrei-drg@users.noreply.github.com>
2022-10-16 20:35:22 +0300

    224

--- Exercitii/index.js
+++ Exercitii/index.js
@@ -7,9 +7,20 @@ console.log(" 'a' a fost declarat")
 
 console.log('valoarea lui a este ${a}')
 
-let a ="10"
+//let a ="10"
 
 console.log("lui a i s-a atribuit 10")
 
 console.log('valoarea lui a este ${a}')
 
+//let a = "20"
+
+console.log("a si-a schimbat valoarea")
+
+console.log('valoarea lui a este ${a}')
+
+let a = "a + 1"
+
+console.log("a si-a marit valoarea cu 1")
+
+console.log('valoarea lui a este ${a}')
