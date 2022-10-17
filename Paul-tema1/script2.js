de59ced575efc450bb19c91daede350ef30a2bb8
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-10-17 17:53:45 +0300

    subtracted 1 from the new value

--- Paul-tema1/script2.js
+++ Paul-tema1/script2.js
@@ -4,8 +4,15 @@ let b = 10;
 
 console.log("b a fost declarat");
 console.log(`valoarea lui b este: ${b}`);
-console.log("lui b i s-a atribuit 10")
+console.log("lui b i s-a atribuit 10");
 
 b = 20;
 
-console.log("b si-a schimbat valoarea")
+console.log("b si-a schimbat valoarea");
+console.log(`valoarea lui b este: ${b}`);
+
+b = b - 1;
+
+console.log("b si-a micsorat valoare cu 1");
+console.log(`valoarea lui b este: ${b}`);
+
