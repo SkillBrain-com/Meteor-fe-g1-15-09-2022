b5033f33c20be536d75cff0ba8192a2bdf0403e5
fposea <fposea@gmail.com>
2022-10-17 22:48:39 +0300

    schimbare a partii de validare

--- script-nivel-1.js
+++ script-nivel-1.js
@@ -1,36 +1,39 @@
 let btnSubmit = document.getElementById("btn-submit");
 btnSubmit.onclick = function() {
     let input = parseInt(document.getElementById("input-number").value);
-    displayData(fizzBuz(input));
-} 
-
-
-function fizzBuz(n) {
-    if (!validate(n)) {
-      return "!!! Number is not valid. Please re-enter a number between 1 and 100 !!!";
+    if (validate(input)) {
+        displayData(fizzBuzz(input));
     }
-    
+    else {
+        let errMessage = "!!! Number is not valid. Please re-enter a number between 1 and 100 !!!";
+        displayData(errMessage);
+    }
+}
+
+
+
+function fizzBuzz(n) {
     let result = "";
-    
+
     for (let i=1; i <= n; i++) {
-      result += "\nn = " + i + "\t >> " + applyRules(i);
+        result += "\nn = " + i + "\t >> " + applyRules(i);
     }
-  
+
     return result;
 }
 
 
 function applyRules(i) {
     if ( i % 15 === 0) {
-      return "FizzBuzz";
+        return "FizzBuzz";
     }
     
     if ( i % 5 === 0) {
-      return "Buzz";
+        return "Buzz";
     }
     
     if ( i % 3 === 0) {
-      return "Fizz";
+        return "Fizz";
     }
     
     return "";
