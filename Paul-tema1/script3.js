0f777344323dcaef24941daf2a2ee53c05ca100e
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-10-17 19:27:30 +0300

    created EXERCITIUL3 declared variables and atributed values to the variables

---
+++ Paul-tema1/script3.js
@@ -0,0 +1,11 @@
+console.log("EXERCITIUL3");
+
+let luni, marti, miercuri, joi, vineri, sambata, duminica;
+
+luni = 0;
+marti = 1;
+miercuri = 2;
+joi = 3;
+vineri = 4;
+sambata = 5;
+duminica = 6;
