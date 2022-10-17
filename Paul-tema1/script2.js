f649646423737881397567785bac57486451ad2a
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-10-17 17:49:53 +0300

    changed value of b

--- Paul-tema1/script2.js
+++ Paul-tema1/script2.js
@@ -5,3 +5,7 @@ let b = 10;
 console.log("b a fost declarat");
 console.log(`valoarea lui b este: ${b}`);
 console.log("lui b i s-a atribuit 10")
+
+b = 20;
+
+console.log("b si-a schimbat valoarea")
