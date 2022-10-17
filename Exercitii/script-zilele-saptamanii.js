27a04ae1cdc50e1400fdb6bcf2ad9529c3c1b458
test.git <112196087+Andrei-drg@users.noreply.github.com>
2022-10-17 08:06:56 +0300

    226
    zilele saptamanii

---
+++ Exercitii/script-zilele-saptamanii.js
@@ -0,0 +1,29 @@
+console.log('EXERCITIUL 3')
+
+let Luni = "0"
+
+console.log("Luni: 0")
+
+let Marti = "1"
+
+console.log("Marti: 1")
+
+let Miercuri = "2"
+
+console.log("Miercuri: 2")
+
+let Joi = "3"
+
+console.log("Joi: 3")
+
+let Vineri = "4"
+
+console.log("Vineri: 4")
+
+let Sambata = "5"
+
+console.log("Sambata: 5")
+
+let Duminica = "6"
+
+console.log("Duminica: 6")
