7556a6649841a3549b41b26b79ca88309dd4bb83
AlinaBogdan <alinabogdan1990@gmail.com>
2022-10-18 20:18:59 +0300

    The third exercise has been finished

---
+++ Javascript-1/script3.js
@@ -0,0 +1,15 @@
+console.log("EXERCITIUL 3");
+let luni = 0;
+console.log("Luni: " + luni);
+let marti = 1;
+console.log("Marti: " + marti);
+let miercuri = 2;
+console.log("Miercuri: " + miercuri);
+let joi = 3;
+console.log("Joi: " + joi);
+let vineri = 4;
+console.log("Vineri: " + vineri);
+let sambata = 5;
+console.log("Sambata: " + sambata);
+let duminica = 6;
+console.log("duminica: " + duminica);
