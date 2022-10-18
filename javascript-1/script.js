ecceaaf123a377401cf2e9f375ffff6742947fe8
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-10-18 11:47:25 +0300

    created variable interpolation

--- javascript-1/script.js
+++ javascript-1/script.js
@@ -4,18 +4,24 @@ let a;
 
 console.log("a a fost declarat")
 
-console.log('valoarea lui a este ${a}');
+console.log(`valoarea lui a este ${a}`);
  
 a=10;
-console.log(a);
+
 
 console.log("lui a i s-a atribuit 10");
 
-console.log('valoarea lui a este ${a}');
+console.log(`valoarea lui a este ${a}`);
 
 
 a=20;
-console.log(a);
+
 
 console.log("a si-a schimbat valoarea");
-console.log('valoarea lui a este ${a}')
+console.log(`valoarea lui a este ${a}`);
+
+a= a+ 1;
+
+console.log("a si-a marit valoarea cu 1");
+
+console.log(`valoarea lui a este ${a}`);
