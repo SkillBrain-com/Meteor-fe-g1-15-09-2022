041c72217a78dbbe4db707f50413b59fde931e40
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-10-18 13:17:07 +0300

    modified script5.js

--- javascript-1/script4.js
+++ javascript-1/script4.js
@@ -1,8 +1,9 @@
 console.log("EXERCITIUL 4");
 
-let nume=any;
 
-let prenume=Georgiana;
+let nume="Radu";
+
+let prenume="Georgiana";
 
 
-console.log("Numele meu este: Georgiana Radu")
+console.log("Numele meu este:Radu Georgiana");
