f1ef93eaf7b389f1a543c0239629aca0a7f78b86
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-10-18 12:38:48 +0300

    created and assigned variable from script4

---
+++ javascript-1/script2.js
@@ -0,0 +1,21 @@
+console.log("EXERCITIUL 2");
+
+let b=10;
+console.log(b);
+
+console.log("b a fost declarat");
+console.log("lui b i s-a atribuit 10");
+
+console.log(`valoarea lui b este ${b}`);
+
+b=20;
+
+
+console.log("b si-a schimbat valoarea");
+console.log(`valoarea lui b este ${b}`);
+
+b=b-5
+
+console.log("b isi micsoreaza valoarea cu 5")
+console.log(`valoarea lui b este ${b}`);
+
---
+++ javascript-1/script4.js
@@ -0,0 +1,8 @@
+console.log("EXERCITIUL 4");
+
+let nume=any;
+
+let prenume=Georgiana;
+
+
+console.log("Numele meu este: Georgiana Radu")
