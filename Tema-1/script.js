45c78d63a4b6c0b194737bf0fa8f2c7cc582c40d
biancaghertan <bianca.ghertan@yahoo.ro>
2022-10-18 14:32:14 +0300

    Added ()

--- Tema-1/script.js
+++ Tema-1/script.js
@@ -3,29 +3,23 @@ console.log("EXERCITIUL 1")
 
 let a;
 
- console.log("a a fost declarat");
+ console.log('a a fost declarat');
 
- console.log("Valoarea lui a este ${a}");
+ console.log(`Valoarea lui a este ${a}`);
 a= 10;
 console.log("Lui a i s-a atribuit 10");
 console.log(a);
  
 
-console.log("Valoarea lui a este ${a}");
+console.log(`Valoarea lui a este ${a}`);
 a= 20;
-console.log("a si-a schimbat valoarea");
+console.log(a si-a schimbat valoarea');
 console.log('Valoarea lui a este ${a}');
 console.log(a)
 
 
 a= a+1;
-console.log("a si-a marit valoarea cu 1");
-console.log("Valoarea lui a este ${a}");
+console.log('a si-a marit valoarea cu 1');
+console.log(`Valoarea lui a este ${a}`);
 console.log(a);
 
-// console.log("EXERCITIUL 4")
-let nume= Ghertan;
-let prenume=Bianca;
-console.log("Numele meu este: ${nume} ${prenume}");
-console.log(nume);
-console.log(prenume);
--- Tema-1/script2.js
+++ Tema-1/script2.js
@@ -3,21 +3,21 @@ console.log("EXERCITIUL 2")
 let b;
 
 console.log("b a fost declarat");
-console.log("Valoarea lui b este ${b}");
+console.log(`Valoarea lui b este ${b}`);
 
 b=10;
 console.log(" Lui b i s-a atribuit valoarea 10");
 console.log(b)
 
-console.log(" Valoarea lui b este ${b}");
+console.log(`Valoarea lui b este ${b}`);
 b= 20;
 
 console.log( "b si-a schimbat valoarea")
-console.log("Valoarea lui b este ${b}");
+console.log(`Valoarea lui b este ${b}`);
 console.log(b)
 
 b= b-5;
 console.log("b si-a miscsorat valoarea cu 5");
-console.log('Valoarea lui b este ${b}');
+console.log(`Valoarea lui b este ${b}`);
 console.log(b)
 
