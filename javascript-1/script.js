fa1ea0caeff4572493b0266407ecf7a12d0cdf09
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-10-18 11:17:09 +0300

    changed the value of the variable

--- javascript-1/script.js
+++ javascript-1/script.js
@@ -6,7 +6,16 @@ console.log("a a fost declarat")
 
 console.log('valoarea lui a este ${a}');
  
-
 a=10;
+console.log(a);
 
-console.log("lui a i s-a atribuit 10")
+console.log("lui a i s-a atribuit 10");
+
+console.log('valoarea lui a este ${a}');
+
+
+a=20;
+console.log(a);
+
+console.log("a si-a schimbat valoarea");
+console.log('valoarea lui a este ${a}')
