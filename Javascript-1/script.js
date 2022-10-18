cc5702a505182138a927dac1b156aac5afce8ea6
AlinaBogdan <alinabogdan1990@gmail.com>
2022-10-18 20:02:34 +0300

    The first exercise has been finished

---
+++ Javascript-1/script.js
@@ -0,0 +1,13 @@
+console.log("EXERCITIUL 1");
+let a;
+console.log("a a fost declarat");
+console.log("valoarea lui a este " + a);
+a = 10;
+console.log("Lui a i s-a atribuit valoarea 10");
+console.log("valoarea lui a este " + a);
+a = a + 10;
+console.log("a si-a schimbat valoarea");
+console.log("Valoarea lui a este " + a);
+a = a + 1;
+console.log("a si-a marit valoarea cu 1");
+console.log("valoarea lui a este " + a);
