6deeb61445a689cecd3ea0b709d89f4ae766ea18
AleBarboni <alexa_wera@yahoo.com>
2022-10-19 09:20:02 +0300

    corected-tema-1

--- JS/Tema 1/script-numele-meu-este.js
+++ JS/Tema 1/script-numele-meu-este.js
@@ -3,4 +3,4 @@ console.log("Exercitiul 4");
 let nume = "Barboni";
 let prenume = "Alexandra";
 
-console.log("Numele meu este:", nume, prenume);
+console.log(`Numele meu este ${nume} ${prenume}`);
--- JS/Tema 1/script.js
+++ JS/Tema 1/script.js
@@ -2,4 +2,16 @@ console.log("EXERCITIUL 1");
 
 let a;
 console.log("a a fost declarat");
-console.log("valoarea lui a este ${a}");
+console.log(`valoarea lui a este ${a}`);
+
+a = 10;
+console.log("lui a i s-a atribuit 10");
+console.log(`valoarea lui a este ${a}`);
+
+a = 20;
+console.log("a si-a schimbat valoarea");
+console.log(`valoarea lui a este ${a}`);
+
+a += 1;
+console.log("a si-a marit valoarea cu 1");
+console.log(`valoarea lui a este ${a}`);
