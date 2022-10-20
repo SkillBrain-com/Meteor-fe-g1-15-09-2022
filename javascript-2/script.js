2a02f75797a2a4a0cfe6e175794f2ba5507525e6
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-20 19:02:19 +0300

    created a condition sequence

--- javascript-2/script.js
+++ javascript-2/script.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+let a, b;
+
+a = 20;
+b = 10;
+
+if (a > b) {
+    console.log(`A(${a}) este mai mare ca B(${b})`);
+}
+else {
+    console.log(`A(${a}) este mai mic ca B(${b})`);
+}
+
+console.log(`Ai ajuns dupa conditie`);
