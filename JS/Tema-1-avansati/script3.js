80e3212cb9a6b1206c82092462f9abc82c847370
AleBarboni <alexa_wera@yahoo.com>
2022-10-20 12:49:55 +0300

    Teme mutate in branch_ul Ale-Barboni-Teme-extra

--- JS/Tema-1-avansati/script3.js
+++ JS/Tema-1-avansati/script3.js
@@ -1,4 +1,5 @@
 console.log("exercitiul-2-avansati")
+console.log("Tema mutata in branch_ul Ale-Barboni-Teme-extra")
 
 let allNumbers = [1, 3, 5, 7, 9, 3, 5, 7, 9, 5, 7, 9, 7, 9, 9];
 
@@ -13,4 +14,3 @@ console.log(`3 -`, countOccurrences(allNumbers, 3), `aparitii`);
 console.log(`5 -`, countOccurrences(allNumbers, 5), `aparitii`);
 console.log(`7 -`, countOccurrences(allNumbers, 7), `aparitii`);
 console.log(`9 -`, countOccurrences(allNumbers, 9), `aparitii`);
-
