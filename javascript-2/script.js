440e9127c890575fb1222d46f9b9dbc08e1cbace
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-20 21:14:02 +0300

    Useed const instead of let

--- javascript-2/script.js
+++ javascript-2/script.js
@@ -1,5 +1,5 @@
-let nume = "Pizza Americana";
-let termenCautare = "Pizza Americana";
-
-let rezultat = nume.includes(termenCautare) === true ? `Rezultatul este ${nume}` : "Nu s-au gasit rezultate";
+const nume = "Pizza Americana";
+const termenCautare = "Pizza Americana";
+const rezultat = nume.includes(termenCautare) === true ? `Rezultatul este ${nume}` : "Nu s-au gasit rezultate";
 console.log(rezultat);
+
