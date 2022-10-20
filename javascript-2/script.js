bb00be76aaa40f7711eec740e30d88b907c03534
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-20 19:21:08 +0300

    Created case for equal values using else if

--- javascript-2/script.js
+++ javascript-2/script.js
@@ -1,16 +1,14 @@
 let a, b;
 
 a = 10;
-b = 100;
+b = 1;
 
 if (a > b) {
     console.log(`A(${a}) este mai mare ca B(${b})`);
 }
+else if (a === b){
+    console.log(`A(${a}) este egal cu B(${b})`);
+}
 else {
-    if (a === b) {
-        console.log(`A(${a}) este egal cu B(${b})`);
-    }
-    else {
-        console.log(`A(${a}) este mai mic ca B(${b})`);
-    }
+    console.log(`A(${a}) este mai mic ca B(${b})`);
 }
