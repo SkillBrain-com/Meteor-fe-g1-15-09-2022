328d862c98a7b8e8c2939bbe8e86af6c4f74f0ff
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-21 18:41:38 +0300

    created DCA method

--- dca/js/main.js
+++ dca/js/main.js
@@ -1,7 +1,28 @@
-const share = {
-    price: 10.00,
-    unitCount: 3,
-};
+const DATA = [
+    {
+        price: 10.00,
+        unitCount: 10,
+    },
+    {
+        price: 12.50,
+        unitCount: 4,
+    },
+    {
+        price: 11.00,
+        unitCount: 2,
+    },
+];
 
-console.log(`Pret ${share.price} lei`);
-console.log(`Unitati ${share.unitCount}`);
+
+function dca(stock) {
+    let sum = 0;
+    let unitSum = 0;
+    for (let i = 0; i < stock.length; i++) {
+        sum += stock[i].price * stock[i].unitCount;
+        unitSum += stock[i].unitCount;
+    }
+    return sum / unitSum;
+}
+
+const result = dca(DATA);
+console.log(result);
