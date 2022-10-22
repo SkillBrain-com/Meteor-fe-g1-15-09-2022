c725f542a4dd2bbfd735be3244da87aef2922196
LazaroiuRazvanGabriel <razvangabriel2602@gmail.com>
2022-10-22 22:19:26 +0300

    Javscript

--- javascript-1/script3.js
+++ javascript-1/script3.js
@@ -1,22 +1,22 @@
 console.log("EXERCITIUL 3");
 
 let luni=0;
-console.log("Luni: 0");
+console.log(`Luni: ${0}`);
 
 let marti=1;
-console.log("Marti: 1");
+console.log(`Marti: ${1}`);
 
 let miercuri=2;
-console.log("Miercuri: 2");
+console.log(`Miercuri: ${2}`);
 
 let joi=3;
-console.log("Joi: 3");
+console.log(`Joi: ${3}`);
 
-let vineri=4;
-console.log("Vineri: 4");
+let vineri=4; 
+console.log(`Vineri: ${4}`);
 
 let sambata=5;
-console.log("Sambata: 5");
+console.log(`Sambata: ${5}`);
 
-let Duminica=6;
-console.log("Duminica: 6");
+let Duminica=6
+console.log(`Duminica: ${6}`);
