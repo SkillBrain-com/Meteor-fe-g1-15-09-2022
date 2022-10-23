e5e7aa81146de3ed76622f0ee84d8aeb43ce611e
AleBarboni <alexa_wera@yahoo.com>
2022-10-23 19:12:28 +0300

    exercitiul 2 rezolvat cu length

---
+++ JS/Tema 2 - Structura decizionala/script2a.js
@@ -0,0 +1,18 @@
+console.log(`Tema 2 - Structura decizionala`);
+console.log(`Exercitiul 2a`);
+
+const x = 12345;
+const lungimeNumar = x.toString().length;
+
+if(lungimeNumar < 3) {
+    console.log(`${x} are mai putin de 3 cifre`);
+    }
+else if(lungimeNumar >= 3 && lungimeNumar <=5  && lungimeNumar % 2 === 0) {
+    console.log(`${x} este par si are intre 3 si 5 cifre`);
+    }
+else if(lungimeNumar >= 3 && lungimeNumar <=5  && lungimeNumar % 2 === 1) {
+    console.log(`${x} este impar si are intre 3 si 5 cifre`);
+}
+else {
+    console.log(`${x} are mai mult de 5 cifre`);
+}
