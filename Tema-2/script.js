7f2790b4b72eb7024ee0af58140b386454c7ad07
Groza Zita <adi_groza@yahoo.com>
2022-10-24 21:36:41 +0300

    created Tema 2 - Exercitiul 1

---
+++ Tema-2/script.js
@@ -0,0 +1,16 @@
+console.log("Tema2 - EXERCITIUL 1");
+
+let x = 123;
+rest = x % 2;
+
+
+ if(rest === 0) {             
+    console.log(`Numarul ${x} este par`);
+    }
+
+    else {
+    console.log(`Numarul ${x} este impar`);
+    }
+    
+    
+     
