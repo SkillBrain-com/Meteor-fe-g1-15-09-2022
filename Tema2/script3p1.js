60399c0f6741ec11e7c4e635b14b33c05cce2f0b
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-10-24 22:20:26 +0300

    created and resolved exercise 3 part 1

---
+++ Tema2/script3p1.js
@@ -0,0 +1,12 @@
+console.log ("Exercitiul 3 partea 1");
+
+const text = "lol";
+
+const lungimeText = text.length;
+console.log (lungimeText);
+
+const primaLitera = text.charAt(0);
+console.log (primaLitera);
+
+const ultimaLitera = text.charAt(text.length -1);
+console.log (ultimaLitera);
