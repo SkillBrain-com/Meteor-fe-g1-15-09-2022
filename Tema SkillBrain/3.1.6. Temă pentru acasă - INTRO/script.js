1554ef1294e31f9e5c2dea92f6490b17f6de09f4
AleBarboni <alexa_wera@yahoo.com>
2022-10-24 15:34:58 +0300

    3.1.6. Temă pentru acasă - INTRO

---
+++ Tema SkillBrain/3.1.6. Temă pentru acasă - INTRO/script.js
@@ -0,0 +1,11 @@
+console.log(`3.1.6. Temă pentru acasă - INTRO`);
+console.log(`Exercitiul 1`);
+
+let a = 150;
+  if(a % 10 === 0) {
+    console.log(`${a} este divizibil cu 10`)
+  }
+  else {
+    console.log(`${a} nu este divizibil cu 10`)
+  }
+ 
