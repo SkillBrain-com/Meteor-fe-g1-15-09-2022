f2428e99892b2339b4edadf25ee766f95ce6aef4
Groza Zita <adi_groza@yahoo.com>
2022-10-24 22:04:34 +0300

    created Tema 2 - Exercitiul 3

---
+++ Tema-2/script3.js
@@ -0,0 +1,18 @@
+console.log("Tema2 - EXERCITIUL 3");
+
+const text = "Adda8"
+
+lungimeText = text.length;
+primaLitera = text.charAt(0);
+ultimaLitera = text.charAt(text.length - 1);
+
+if (
+	primaLitera >= "a" && primaLitera <= "z" ||     //textul inceape cu o litera mica 
+	ultimaLitera >= "0" && ultimaLitera <= "9" &&  // SAU textul inceape cu o litera mica SAU se termina cu o cifra
+	lungimeText >= 3 && lungimeText <= 5          //textul este mai lung de 3 caractere si mai scurt de 5 caractere
+    )
+	console.log (`Textul "${text}" incepe cu o litera mica sau se termina cu o cifra si are intre 3 si 5 caractere`);
+
+else {
+    console.log (`Textul "${text}" nu respecta regulile`);
+}
