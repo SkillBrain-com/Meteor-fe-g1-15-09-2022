34e190795e08a81acfec5458e9d38a5498e499de
ovidiufilip10 <ovidiufilip10@yahoo.com>
2022-10-25 20:12:46 +0300

    Nivel 2 remake

--- Tema 2/nivel2.js
+++ Tema 2/nivel2.js
@@ -1,14 +1,22 @@
-const input = [1,5,6,6,7,8,8,1,5,6,9,9,0,2,1,4,5,8];
-const output = [0,0,0,0,0,0,0,0,0,0];
+const input = [1,5,6,24,7,8,8,141,32,14,11,5,6,9,39,11,2,1,54,5,8];
+const output = [];
 let i=0;
-while(input[i] !== undefined){
+for(i=0;i<=input.length;i++){
+if(input[i] !== 0 &&  output[input[i]] === undefined){
+    output[input[i]]=0;
     output[input[i]]++;
-    i++;
+}
+else if(input[i] !== 0){
+    output[input[i]]++;
+}
 }
 i=0;
-while(output[i] !== undefined){
+for(i=0;i<=output.length;i++){
+    if(output[i] !== 0 && output[i] !== undefined){
+
     console.log(`${i} - ${output[i]} aparitii`);
     i++;
+}
 }
 
     
