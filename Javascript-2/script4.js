b13e627f84200fe10bb8c9a89e8de0141ebf5246
AlinaBogdan <alinabogdan1990@gmail.com>
2022-10-25 22:11:28 +0300

    First changes for exercise 4 done

--- Javascript-2/script4.js
+++ Javascript-2/script4.js
@@ -1,16 +1,19 @@
-const text = "Fall";
+const text = "AutumnF4";
 const lungimeText = text.length;
 const contineLiteraF = text.includes("F");
 const primaLitera = text.charAt(0);
 const aDouaLitera = text.charAt(1);
-const aTreiaLitera = text.charAt(2);
-const aPatraLitera = text.charAt(3);
 
-if (text.length === 8) {
-    if (contineLiteraF) {
-        if (primaLitera >= 0 || aDouaLitera >= a) {
-            console.log(`Textul ${text} are lungimea 8, contine caracterul F si prima litera a textului este o cifra sau a doua litera este o litera`)
+
+if (
+    (text.length === 8) &&
+    (contineLiteraF) &&
+    (primaLitera >= 0 || aDouaLitera >= "a") 
+    ) {
+        console.log(`Textul ${text} are lungimea 8, contine caracterul F si prima litera a textului este o cifra sau a doua litera este o litera`)
         }
-    }
+    
+
+else {
+    console.log(`Textul ${text} nu respecta regulile`)
 }
-else console.log (`Textul ${text} nu respecta regulile`)
