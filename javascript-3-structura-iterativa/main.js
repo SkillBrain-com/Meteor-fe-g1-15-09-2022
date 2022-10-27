6aa4f715f77d5e86ddf1568be4c8a0eb6408dbde
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-27 21:30:24 +0300

    js-3 update: filtered average temperatures

--- javascript-3-structura-iterativa/main.js
+++ javascript-3-structura-iterativa/main.js
@@ -3,10 +3,26 @@ console.log("Structura itertiva si liste");
 const wheatherMaxTempList = [16, 17, 17, 18, 17, 18, 19, 18];
 const wheatherMinTempList = [4, 4, 6, 7, 5, 6, 6, 5];
 const wheatherDayList = ["Luni", "Marti", "Miercuri", "Joi", "Vineri", "Sambata", "Duminica", "Luni"];
+const results = [];
 
 let sumTempMax = 0;
 let sumTempMin = 0;
-let sumTempAvg = 0;
+
+for(let i = 0; i < wheatherMaxTempList.length; i++) {
+    const maxOfTheDay = wheatherMaxTempList[i];
+    const minOfTheDay = wheatherMinTempList[i];
+    sumTempMax += maxOfTheDay;
+    sumTempMin += minOfTheDay;
+}
+
+const avgTempMax = sumTempMax / wheatherMaxTempList.length;
+const avgTempMin = sumTempMin / wheatherMinTempList.length;
+const avgTempAvg = (avgTempMax + avgTempMin) / 2;
+
+console.log(`Media maximelor este ${avgTempMax}°C`);
+console.log(`Media minimelor este ${avgTempMin}°C`);
+console.log(`Media mediilor este ${avgTempAvg}°C`);
+
 
 for(let i = 0; i < wheatherMaxTempList.length; i++) {
     const maxOfTheDay = wheatherMaxTempList[i];
@@ -15,18 +31,13 @@ for(let i = 0; i < wheatherMaxTempList.length; i++) {
     sumTempMax += maxOfTheDay;
     sumTempMin += minOfTheDay;
     
-    const averageTemp = (maxOfTheDay + minOfTheDay) / 2;
-    sumTempAvg += averageTemp;
+    const averageOfTheDay = (maxOfTheDay + minOfTheDay) / 2;
 
-    if (averageTemp > 0) {
-        console.log(`${wheatherDayList[i]}: Max:${maxOfTheDay}°C min:${minOfTheDay}°C avg:${averageTemp}°C`);
+    if (averageOfTheDay > avgTempAvg) {
+        console.log(`${wheatherDayList[i]}: Max:${maxOfTheDay}°C min:${minOfTheDay}°C avg:${averageOfTheDay}°C`);
+        results.push(averageOfTheDay);
     }
 }
 
-const avgTempMax = sumTempMax / wheatherMaxTempList.length;
-const avgTempMin = sumTempMin / wheatherMinTempList.length;
-const avgTempAvg = sumTempAvg / wheatherMaxTempList.length;
-
-console.log(`Media maximelor este ${avgTempMax}°C`);
-console.log(`Media minimelor este ${avgTempMin}°C`);
-console.log(`Media mediilor este ${avgTempAvg}°C`);
+console.log(`Exista ${results.length} zile care au temperatura mai mare decat media din aceasta perioada (${avgTempAvg})°C`);
+console.log(results);
