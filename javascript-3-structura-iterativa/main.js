873ad84e56b6b54f0893c70257dcba23d9fcaba6
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-27 20:09:03 +0300

    js-3 update: random guessing game

--- javascript-3-structura-iterativa/main.js
+++ javascript-3-structura-iterativa/main.js
@@ -4,13 +4,13 @@ console.log("Structura itertiva");
 
 //numar la intamplare intre 0 si 10
 const secret = Math.round(Math.random() * 10);
-let contor = 0;
+let guess = 0;
+let numarIncercari = 0;
 
-while (contor !== secret) {
-    contor += 1;
+while (guess !== secret) {
+    guess = Math.round(Math.random() * 10);
+    numarIncercari++;
 }
 
-console.log(`Cred ca secretul era ${contor}`);
+console.log(`Cred ca secretul era ${guess}. Am facut ${numarIncercari} incercari`);
 console.log(`Secretul este ${secret}`);
-
-
