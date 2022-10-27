ad721282316d454a77f07b71fda908bd9e6eb36b
biancaghertan <bianca.ghertan@yahoo.ro>
2022-10-27 13:47:38 +0300

    Added changes

--- Tema-2/script3.js
+++ Tema-2/script3.js
@@ -1,8 +1,28 @@
 console.log ('Exercitiul 3')
 
-const text= "Cana"
-const lungimeText= text.length;
-console.log(lungimeText);
+
+const text= " Acesta e un text";
+
+const lungimeText1= text.length;
+const primaLitera1= text.charAt(0);
+const ultimaLitera1= text.charAt(text.length);
+
+if ( primaLitera1 >= "a"  && 
+primaLitera1 <= "z" || ultimaLitera1 >= "0" && ultimaLitera1 <="9" && lungimeText1 >= 3 && lungimeText1 <= 5  ) {
+    console.log(`Textul ${text} incepe cu o litera mica sau se termina cu o cifra intre 3 si 5 caractere`)
+
+}
+else {
+    console.log(`Textul ${text} nu respecta regulile`);
+
+}
+console.log(lungimeText1);
+console.log(primaLitera1);
+console.log(ultimaLitera1);
 
 
-const text2=  
+
+
+
+
+
