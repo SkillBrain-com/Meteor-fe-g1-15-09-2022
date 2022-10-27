961b6c77e2255453d816b9006bb333960819804d
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-27 20:26:14 +0300

    js-3 update: selected second element from a list

--- javascript-3-structura-iterativa/main.js
+++ javascript-3-structura-iterativa/main.js
@@ -2,10 +2,6 @@ console.log("Structura itertiva si liste");
 
 const list = []; // lista goala
 list.push(10);
-console.log(list);
-
 list.push(20);
-console.log(list);
-
 list.push(30);
-console.log(list);
+console.log(list[1]);
