43e66becabb4b1d6eb1b47b92bf9310d815cd76d
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-27 19:31:07 +0300

    js-3 add: created while loop

--- javascript-3-structura-iterativa/main.js
+++ javascript-3-structura-iterativa/main.js
@@ -1,1 +1,17 @@
+/*
+Algoritm in care sa AFISAM un numar cu cel putin 10 cifre si cel mult 1000 de cifre.
+Numarul va reprezenta o putere a lui 10; Numarul trebuie sa fie afisat cu toate cifrele.
+*/
 console.log("Structura itertiva");
+
+const n = 100;
+let textNumar = "1";
+let contor = 0;
+
+while (contor < n) {
+    textNumar += "0";
+    console.log(`Contorul este ${contor}`);
+    contor++;
+}
+
+console.log(textNumar);
