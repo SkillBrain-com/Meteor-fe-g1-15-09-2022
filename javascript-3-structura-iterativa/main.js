3da336daf3a241ef9b6dd6403b767de6a605af7a
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-27 20:14:30 +0300

    js-3 update: created sum algorithm

--- javascript-3-structura-iterativa/main.js
+++ javascript-3-structura-iterativa/main.js
@@ -1,16 +1,9 @@
-// o functie (in programare) este un programel gata facut, 
-// pe care il putem folosi prin ai spune numele urmat de niste paranteze
 console.log("Structura itertiva");
 
-//numar la intamplare intre 0 si 10
-const secret = Math.round(Math.random() * 10);
-let guess = 0;
-let numarIncercari = 0;
+let suma = 0;
 
-while (guess !== secret) {
-    guess = Math.round(Math.random() * 10);
-    numarIncercari++;
+for (let i = 0; i < 100; i++) {
+    suma += i;
 }
 
-console.log(`Cred ca secretul era ${guess}. Am facut ${numarIncercari} incercari`);
-console.log(`Secretul este ${secret}`);
+console.log(suma);
