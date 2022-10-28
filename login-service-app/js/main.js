5957a8e436bd43fb229ba48e7d61b644b5c810d5
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-28 18:39:21 +0300

    extra-2 update: changeRoute copy paste variant

--- login-service-app/js/main.js
+++ login-service-app/js/main.js
@@ -1,10 +1,35 @@
-let selectedPage = "LOGIN";
+const ROUTES = {
+    LOGIN: "LOGIN",
+    USER: "USER"
+};
 
+/* LOGIN PAGE VARS */
 const loginPage = document.querySelector("#login-page");
+const loginButton = document.querySelector("#login-button");
+
+/* USER PAGE VARS */
 const userPage = document.querySelector("#user-page");
-const toggleButton = document.querySelector("#toggle-button");
 
 
-toggleButton.addEventListener("click", function(){
-    loginPage.classList.toggle("hidden");
+let selectedPage = ROUTES.LOGIN;
+
+loginButton.addEventListener("click", function(){
+    selectedPage = ROUTES.USER;
+    
+    if (selectedPage === ROUTES.LOGIN) {
+        loginPage.classList.toggle("hidden");
+        userPage.classList.toggle("hidden");
+    }
+    else if (selectedPage === ROUTES.USER){
+        loginPage.classList.toggle("hidden");
+        userPage.classList.toggle("hidden");
+    }
 });
+
+
+if (selectedPage === ROUTES.LOGIN) {
+    loginPage.classList.toggle("hidden");
+}
+else if (selectedPage === ROUTES.USER){
+    userPage.classList.toggle("hidden");
+}
