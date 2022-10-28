dd1e0dee60dd838ab437cfdf09ab2c6b927546c7
cezarmocanu <cezarmocanu2014@gmail.com>
2022-10-28 18:45:43 +0300

    change route system

--- login-service-app/js/main.js
+++ login-service-app/js/main.js
@@ -10,26 +10,24 @@ const loginButton = document.querySelector("#login-button");
 /* USER PAGE VARS */
 const userPage = document.querySelector("#user-page");
 
+//nextPage - pagina la care se trece
+function chnagePage(nextPage){
+    if (nextPage === ROUTES.LOGIN) {
+        loginPage.classList.remove("hidden");
+        userPage.classList.add("hidden");
+    }
+    else if (nextPage === ROUTES.USER){
+        loginPage.classList.add("hidden");
+        userPage.classList.remove("hidden");
+    }
+}
 
 let selectedPage = ROUTES.LOGIN;
 
+
 loginButton.addEventListener("click", function(){
     selectedPage = ROUTES.USER;
-    
-    if (selectedPage === ROUTES.LOGIN) {
-        loginPage.classList.toggle("hidden");
-        userPage.classList.toggle("hidden");
-    }
-    else if (selectedPage === ROUTES.USER){
-        loginPage.classList.toggle("hidden");
-        userPage.classList.toggle("hidden");
-    }
+    chnagePage(selectedPage);    
 });
 
-
-if (selectedPage === ROUTES.LOGIN) {
-    loginPage.classList.toggle("hidden");
-}
-else if (selectedPage === ROUTES.USER){
-    userPage.classList.toggle("hidden");
-}
+chnagePage(selectedPage);
