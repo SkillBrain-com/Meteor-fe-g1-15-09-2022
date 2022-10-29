97d188b49e3c52277d0e2378c770a3d0d3999879
test.git <112196087+Andrei-drg@users.noreply.github.com>
2022-10-29 12:57:59 +0300

    first exercices

---
+++ Tema 2/exercitiu1.js
@@ -0,0 +1,12 @@
+console.log("Tema 2 - exercitiu 1")
+
+let z = 123;
+const rest = z % 2;
+
+if(rest === 0) {
+    console.log(`numarul ${z} este par`)
+}
+
+else {
+    console.log(`numarul ${z} este impar`);
+    }
