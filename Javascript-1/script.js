c61db9b7015c503b7f0179a80aa90c7ab8639e4f
Livia Soare <livia.soare07@yahoo.com>
2022-10-29 20:38:08 +0300

    Tema 1

---
+++ Javascript-1/script.js
@@ -0,0 +1,72 @@
+console.log("EXERCITIUL 1")
+let a;
+console.log("a a fost declarat");
+console.log("Valoarea lui a este ${a}");
+a = 10 ;
+console.log(a)
+console.log("lui a i s-a atribuit valoarea 10")
+
+console.log("Valoarea lui a este 10");
+a = 20;
+console.log(a);
+console.log("a si a schimbat valoarea")
+console.log("Valoarea lui a este 20");
+a = a + 1;
+console.log(a)
+console.log("a si a marit valorea cu 1")
+console.log("Valoarea lui a este 21")
+
+console.log("EXERCITIUL 2")
+let b;
+console.log("b a fost declarat");
+console.log("Valoarea lui a este ${b}");
+b = 10;
+console.log(b)
+console.log("lui b i s-a atribuit valoarea 10")
+
+console.log("Valoarea lui b este 10");
+b = 20;
+console.log(b);
+console.log("b si-a schimbat valoarea")
+console.log("Valoarea lui b este 20");
+b = b - 5;
+console.log(b)
+console.log("b si-a micsorat  valorea cu 5")
+console.log("Valoarea lui b este 15")
+
+
+console.log("EXERCITIUL 3")
+let zi1
+Zi1 = "Luni: 1"
+console.log(Zi1)
+
+let zi2
+Zi2 = "Marti: 2"
+console.log(Zi2)
+
+let zi3
+Zi3 = "Miercuri: 3"
+console.log(Zi3)
+
+let zi4
+Zi4 = "Joi: 4"
+console.log(Zi4)
+
+let zi5
+Zi5 = "Vineri: 5"
+console.log(Zi5)
+
+let zi6
+Zi6 = "Sambata: 6"
+console.log(Zi6)
+
+let zi7
+Zi7 = "Duminica: 7"
+console.log(Zi7)
+
+console.log("EXERCITIUL 4")
+let Nume = "Soare"
+let Prenume = "Livia"
+console.log("Numele meu este:" + Nume + " " + Prenume)
+
+
