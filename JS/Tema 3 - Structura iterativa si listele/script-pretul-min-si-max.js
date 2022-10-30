9a1374a73a19a28ebe2063e733cd1594e24d380c
AleBarboni <alexa_wera@yahoo.com>
2022-10-30 06:55:31 +0200

    added tema 3, cerinta 3

---
+++ JS/Tema 3 - Structura iterativa si listele/script-pretul-min-si-max.js
@@ -0,0 +1,17 @@
+console.log(`Tema 3`);
+console.log(`Cerinta 3`);
+
+const carBuyPrice2 = [16000, 9000, 17000, 20300, 6000, 8000, 10000, 6000, 35500, 18900, 41000, 22500, 20500, 11590];
+
+for(i = 0; i < carBuyPrice2.length; i++) {
+const carBuyPriceMax = Math.max(carBuyPrice2[i]);
+console.log(carBuyPriceMax);
+
+const carBuyPriceMin = Math.min(carBuyPrice2[i]);
+console.log(carBuyPriceMin);
+}
+
+
+    
+   
+    
