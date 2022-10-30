b2700f38d3801b671e0d8b6b1f16c949aa3b13f1
AleBarboni <alexa_wera@yahoo.com>
2022-10-30 05:26:51 +0200

    created and finished cerinta 1

---
+++ JS/Tema 3 - Structura iterativa si listele/script-marca-si-model.js
@@ -0,0 +1,6 @@
+const carMake = ["BMW", "Audi", "Dacia", "Audi", "Audi", "Dacia", "Dacia", "Dacia", "BMW", "Volkswagen", "Opel", "Mercedes", "BMW", "Volkswagen"]
+const carModel = ["Seria 1", "A3", "Spring", "Q5", "A4", "Spring", "Docker", "Logan", "X5", "Golf", "Astra", "E 200", "220i", "Golf"]
+
+for(let i = 0; i < carMake.length; i++) {
+    console.log(`${carMake[i]} ${carModel[i]}`);
+}
