10659b97c6eb4d4b8a4f21ce0d5f8324326c0f8d
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-10-30 11:43:33 +0200

    created a condition

---
+++ Javascript-2/script.js
@@ -0,0 +1,12 @@
+console.log("structura decizionala-tema 2");
+
+const numar=123;
+const rest= numar% 2;
+
+if(rest===0){
+    console.log(`Numarul ${numar} este par`);
+}
+
+else{
+    console.log(`Numarul ${numar} este impar`);
+}
