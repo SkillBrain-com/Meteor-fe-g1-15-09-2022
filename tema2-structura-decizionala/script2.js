9837e329920525cd9188aa96d7f8d97cd6b4a92a
gabrielapaica <esteragabriela3@gmail.com>
2022-10-31 14:29:13 +0200

    put condition and the number has less than 3 digits

--- tema2-structura-decizionala/script2.js
+++ tema2-structura-decizionala/script2.js
@@ -1,17 +1,4 @@
-
- let numar = 123;
- if(numar >= 0 && numar < 100) {
-    console.log(`${numar} are mai putin de 3 cifre`);
- }
- else if(numar >= 100 && numar < 100000 && numar % 2 === 0) {
-   console.log(`${numar} este par si are intre 3 si 5 cifre`);
+const number = 123;
+if(number >= 0 && number < 100) {
+   console.log(`${number} are mai putin de 3 cifre`);
    }
-   else if(numar >= 100 && n < 100000 && numar % 2 === 1) {
-      console.log(`${numar} este impar si are intre 3 si 5 cifre`);
-      }
-      else if (numar >= 100000 ) {
-         console.log(`${numar} are mai mult de 5 cifre`);
-         }
-         else {
-        console.log(`numarul ${numar} nu se incadreaza in nici un criteriu`);
-        }
