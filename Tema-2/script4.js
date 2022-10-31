61c8c7d71ea956fa6ca23e84d1a65eccd1dc29e6
biancaghertan <bianca.ghertan@yahoo.ro>
2022-10-31 19:01:41 +0200

    delete ;/script 4

--- Tema-2/script4.js
+++ Tema-2/script4.js
@@ -8,7 +8,7 @@ const contineLitera= text1.includes("@");
  const aDouaLitera= text1.charAt(1);
 
 
- if ( lungimeText ===8 && contineLitera=== true && primaCifra >= "0" && primaCifra <="9" || aDouaLitera >="a" && aDouaLitera <="z"); {
+ if ( lungimeText ===8 && contineLitera=== true && primaCifra >= "0" && primaCifra <="9" || aDouaLitera >="a" && aDouaLitera <="z") {
     console.log(` Textul ${text} respecta regulile`);
  }
 
