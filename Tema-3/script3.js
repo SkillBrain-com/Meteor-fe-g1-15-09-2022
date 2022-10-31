c00d85e90fe96c9c236ad6c0180b8136f69a8dea
biancaghertan <bianca.ghertan@yahoo.ro>
2022-11-01 00:50:52 +0200

    failed script3

---
+++ Tema-3/script3.js
@@ -0,0 +1,13 @@
+console.log("Exercitiul 3");
+
+const cumpararePretList=[16000, 9000, 20300, 6000, 8000, 10000, 6000, 35500, 18900, 41000, 22500, 20500, 11590];
+const cumpararePretMax= cumpararePretList;
+const cumpararePretMin= cumpararePretList;
+for (i=0; i < cumpararePretList.length; i++) {
+
+if (cumpararePretMax <= cumpararePretList.length[i])
+ 
+  if(  cumpararePretMin >= cumpararePretList.length[i]);
+}
+
+console.log(`Max este ${cumpararePretMax} si min este ${cumpararePretMin}`);
