ec3b6025ac0fb93b17a9c254b96016cde9297d5b
AleBarboni <alexa_wera@yahoo.com>
2022-10-31 18:12:30 +0200

    added extra to cerinta 4

--- JS/Tema 3 - Structura iterativa si listele/script-car-transmission.js
+++ JS/Tema 3 - Structura iterativa si listele/script-car-transmission.js
@@ -5,6 +5,9 @@ console.log(`Cerinta 4`);
 //au transmisie automata si cate au transmisie manuala.
 
 const carTransmission = ["Automata", "Manuala", "Automata", "Automata", "Automata", "Manuala", "Manuala", "Manuala", "Automata", "Manuala", "Automata", "Automata", "Manuala", "Automata"];
+const carMake3 = ["BMW", "Audi", "Dacia", "Audi", "Audi", "Dacia", "Dacia", "Dacia", "BMW", "Volkswagen", "Opel", "Mercedes", "BMW", "Volkswagen"];
+const carModel3 = ["Seria 1", "A3", "Spring", "Q5", "A4", "Spring", "Docker", "Logan", "X5", "Golf", "Astra", "E 200", "220i", "Golf"];
+
 
 let manualTrasmission = 0;
 let automaticTransmission = 0;
@@ -21,3 +24,14 @@ for (let i = 0; i < carTransmission.length; i++) {
    }
 }
 console.log(`Sunt ${manualContor} masini cu transmisie manuala si ${automaticContor} masini cu transmisie automata.`);
+
+for (let i = 0; i < carTransmission.length; i++) {
+   if(carTransmission[i] === "Manuala" || carTransmission[i] === "Automata") {
+      manualContor++;
+      console.log(`Masina ${carMake3[i]} ${carModel3[i]} are trsansmisia ${carTransmission[i]}.`);
+   }
+}
+
+
+
+
