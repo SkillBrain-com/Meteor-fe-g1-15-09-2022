5ff0ea46240f213bcef721b9cd46733ee4dec04b
biancaghertan <bianca.ghertan@yahoo.ro>
2022-10-31 23:14:08 +0200

    Exercitiul 1

--- Tema-3/script1.js
+++
@@ -1,8 +0,0 @@
-console.log("Exercitiul 1")
-
-const marcaList= [BMW, Audi, Dacia, Audi, Audi, Dacia, Dacia, Dacia, BMW, Volkswagen, Opel, Mercedes, BMW,  Volkswagen];
-const modelList=[Seria 1, A3, Spring, Q5, A4, Spring, Docker, Logan, X5, Golf, Astra, E 200, 220i, Golf];
-
-for (let i =0; i< marcaList.length; i++){
-console.log( `${marcaList} ${modelList}`)
-}
