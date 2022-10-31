e07d52ec6ddc18dbd9d56399e4eb1a6445b786ce
biancaghertan <bianca.ghertan@yahoo.ro>
2022-10-31 23:21:12 +0200

    Changes index.html and script1

--- Tema-3/script1-3.js
+++ Tema-3/script1-3.js
@@ -1,2 +1,12 @@
 console.log("Exercitiul 1");
 
+const marcaList= ["BMW", "Audi", "Dacia", "Audi", "Audi", "Dacia", "Dacia", "Dacia", "BMW", "Volkswagen", "Opel", "Mercedes", "BMW", "Volkswagen"];
+const modelList=["Seria 1", "A3", "Spring", "Q5", "A4", "Spring", "Docker", "Logan", "X5", "Golf", "Astra", "E 200", "220i", "Golf"];
+
+
+for (let i = 0; i <  marcaList.length; i++){
+console.log( `${marcaList[i]}   ${modelList[i]}`)
+
+}
+
+console.log(marcaList);
