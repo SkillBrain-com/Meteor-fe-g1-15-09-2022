3ad09c98ac8eb8f3f09f1639dc0be18c10eac486
gabrielapaica <esteragabriela3@gmail.com>
2022-10-31 14:43:10 +0200

    number este par si are intre 3 si 5 cifre

--- tema2-structura-decizionala/script2.js
+++ tema2-structura-decizionala/script2.js
@@ -2,3 +2,9 @@ const number = 123;
 if(number >= 0 && number < 100) {
    console.log(`${number} are mai putin de 3 cifre`);
    }
+   else if(number >= 100 && number < 10000 && number % 2 === 0) {
+      console.log(`${number} este par si are intre 3 si 5 cifre`);
+      }
+      else if(number >= 100 && number < 10000 && number % 2 === 1) {
+         console.log(`${number} este impar si are intre 3 si 5 cifre`);
+         }
