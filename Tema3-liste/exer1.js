a90d041c6b1706920ee481ad14969c6eac8bc7a1
ovidiufilip10 <ovidiufilip10@yahoo.com>
2022-10-31 19:40:53 +0200

    Exercitiul 1

---
+++ Tema3-liste/exer1.js
@@ -0,0 +1,6 @@
+const marca = ['BMW','Audi','Dacia','Audi','Audi','Dacia','Dacia','Dacia','BMW','Volkswagen','Opel','Merceds','BMW','Volkswagen'];
+const model = ['Seria 1','A3','Spring','Q5','A4','Spring','Docker','Logan','X5','Golf','Astra','E 200','220i','Golf'];
+for(let i=0; i < marca.length; i++){
+    console.log(`${marca[i]} ${model[i]}\n`);
+}
+console.log(marca.length)
