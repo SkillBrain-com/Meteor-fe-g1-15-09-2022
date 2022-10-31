989afeb3e7943a8b213eee92ae4a7251a7c84189
gabrielapaica <esteragabriela3@gmail.com>
2022-10-31 14:51:16 +0200

    I changed the condition. It should have been 100000, not 10000

--- tema2-structura-decizionala/script2.js
+++ tema2-structura-decizionala/script2.js
@@ -2,10 +2,10 @@ const number = 123;
 if(number >= 0 && number < 100) {
    console.log(`${number} are mai putin de 3 cifre`);
    }
-   else if(number >= 100 && number < 10000 && number % 2 === 0) {
+   else if(number >= 100 && number < 100000 && number % 2 === 0) {
       console.log(`${number} este par si are intre 3 si 5 cifre`);
       }
-      else if(number >= 100 && number < 10000 && number % 2 === 1) {
+      else if(number >= 100 && number < 100000 && number % 2 === 1) {
          console.log(`${number} este impar si are intre 3 si 5 cifre`);
          }
          else if (number >= 100000 ) {
