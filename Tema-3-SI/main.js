b6819c53e785c5ed7b9c222c4d8e261be9ebfde1
Groza Zita <adi_groza@yahoo.com>
2022-10-31 18:18:37 +0200

    Created Tema 3 - ex. 1

--- Tema-3-SI/main.js
+++ Tema-3-SI/main.js
@@ -1,1 +1,9 @@
 console.log ("Tema 3 - Structura iterativa");
+
+const marcaList = ["BMW", "Audi", "Dacia", "Audi", "Audi", "Dacia", "Dacia", "Dacia", "BMW", "Volkswagen", "Opel", "Mercedes", "BMW", "Volkswagen"];
+const modelList = ["Seria 1", "A3", "Spring", "Q5", "A4", "Spring", "Docker", "Logan", "X5", "Golf", "Astra", "E 200", "220i", "Golf"];
+
+console.log ("Punctul 1");
+for (let i = 0; i < marcaList.length; i++) {
+    console.log (`${marcaList[i]} ${modelList[i]}`);
+}
