edad4803ff27be977b9076a304eb167aa9fcc2bb
gabrielapaica <esteragabriela3@gmail.com>
2022-10-31 14:46:05 +0200

    the number has more then 5 digits

--- tema2-structura-decizionala/script2.js
+++ tema2-structura-decizionala/script2.js
@@ -8,3 +8,6 @@ if(number >= 0 && number < 100) {
       else if(number >= 100 && number < 10000 && number % 2 === 1) {
          console.log(`${number} este impar si are intre 3 si 5 cifre`);
          }
+         else if (number >= 100000 ) {
+            console.log(`${number} are mai mult de 5 cifre`);
+            }
