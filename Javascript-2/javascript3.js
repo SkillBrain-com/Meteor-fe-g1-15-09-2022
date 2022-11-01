f7031a5498e77bdb05cb238a7ec60e70b3cc3e88
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-11-01 09:41:54 +0200

    added and defined a new variable and added changes

--- Javascript-2/javascript3.js
+++ Javascript-2/javascript3.js
@@ -6,3 +6,12 @@ const primaLitera=text.charAt(0);
 console.log(lungimeText);
 console.log(primaLitera);
 
+const text1="Acesta este un text";
+const lungimeText1=text1.length;
+const primaLitera1=text1.charAt(0);
+const ultimaLitera1=text1.charAt(text.length)
+
+console.log(lungimeText1);
+console.log(primaLitera1);
+console.log(ultimaLitera1);
+
