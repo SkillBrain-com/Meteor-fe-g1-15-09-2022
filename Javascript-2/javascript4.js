69903aa98a4e3272f850fccf26c8f89340116c9e
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-11-01 10:56:18 +0200

    created exercise 4 and added changes

--- Javascript-2/javascript4.js
+++ Javascript-2/javascript4.js
@@ -1,1 +1,21 @@
 console.log("EXERCITIUL 4");
+
+const text2="Hello world";
+const lungimeText2=text2.length
+const contineLitera=text2.includes("@");
+const primaCifra=text2.charAt(0);
+const aDouaLitera= text2.charAt(1);
+
+console.log(text2);
+console.log(lungimeText2);
+console.log(contineLitera); 
+console.log(primaCifra);
+console.log(aDouaLitera);
+
+if (lungimeText2===8 && primaCifra>="0" && primaCifra<="9"
+ || aDouaLitera>="a" && aDouaLitera<="z"){
+    console.log(`Textul ${text2} respecta regulile`);
+ }
+else {
+    console.log(`Textul ${text2} nu respecta regulile`);
+}
