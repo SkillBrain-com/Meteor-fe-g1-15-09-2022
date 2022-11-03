284b89c60197caf665da40648a11b8db6455c606
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-03 20:09:54 +0200

    js-4 update: generated cars using math random

--- javascript-4-dictionare/data.js
+++ javascript-4-dictionare/data.js
@@ -1,7 +1,16 @@
-const dimensiuneEsantion = 100;
+const DIMENSIUNE_ESANTION = 1500;
+const MARCI = ["BMW", "Audi", "Opel", "Dacia", "Toyota", "Honda", "Subaru"];
+
 const marciMasini = [];
 
-for(let i = 0; i < dimensiuneEsantion; i++) {
-    marciMasini.push("Audi");
+for(let i = 0; i < DIMENSIUNE_ESANTION; i++) {
+    const lungimeLista = MARCI.length - 1;
+
+    //Math.random() -> genereaza un numar cu virgula intre 0 si 1
+    //Math.random() * 1000 -> genereaza un numar cu virgula intre 0 si 1000
+    //Math.round(Math.random() * 1000) -> genereaza un numar fara virgula intre 0 si 1000
+    //Math.round(Math.random() * (MARCI.length - 1)) -> genereaza o pozitie la intamplare a unui element din lista MARCI
+    const numarLaIntamplare = Math.round(Math.random() * lungimeLista);
+    marciMasini.push(MARCI[numarLaIntamplare]);
 }
 
