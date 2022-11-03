264e5448e1d2fcec34489a05d4f0bddf9b285fc3
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-03 19:10:02 +0200

    js-4 update: search by key in variable

--- javascript-4-dictionare/main.js
+++ javascript-4-dictionare/main.js
@@ -1,5 +1,4 @@
 const carteTelefoane = {
-    cezarMocanu: "13241",
     "cezar mocanu": "0770000001",
     "vlad anghel": "0770000002",
     "miruna maria": "0770000003",
@@ -7,7 +6,8 @@ const carteTelefoane = {
 
 console.log(carteTelefoane);
 
-const numarTelefonCautat = carteTelefoane["cezar mocanu"];
-const numarTelefonCautat2 = carteTelefoane.cezarMocanu;
-console.log(`Numarul de telefon a lui cezar mocanu este ${numarTelefonCautat} sau ${numarTelefonCautat2}`);
+const nume = "cezar mocanu";
+
+const numarTelefonCautat = carteTelefoane[nume];
+console.log(`Numarul de telefon a lui cezar mocanu este ${numarTelefonCautat}`);
 
