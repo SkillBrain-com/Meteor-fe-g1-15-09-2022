1d6730368699b91cdf3eb3ef6d5b698c0ac25d6c
LazaroiuRazvanGabriel <razvangabriel2602@gmail.com>
2022-11-03 21:34:10 +0200

    Exercitiul -1

---
+++ javascript-1/Structura Iterativa Si Listele/script.js
@@ -0,0 +1,7 @@
+const marca = ["BMW", "Audi", "Dacia", "Audi", "Audi", "Dacia", "Dacia", "Dacia", "BMW", "Volkswagen", "Opel", "Mercedes", "BMW", "Volkswagen"];
+const model = ["Seria 1", "A3", "Spring", "Q5", "A4", "Spring", "Docker", "Logan", "X5", "Golf", "Astra", "E 200", "220i", "Golf"];
+
+for (let i = 0; i < marca.length; i++) {
+    console.log(marca[i] + " " + model[i]);
+    // console.log (`${marca[i]} ${model[i]}`);
+}
