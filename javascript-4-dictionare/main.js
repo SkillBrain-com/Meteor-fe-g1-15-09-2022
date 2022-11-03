5d180c8d60d2638a19613809d2062163e9c8ee6a
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-03 19:00:57 +0200

    js-4 update: created simple dictionary and accesed value

--- javascript-4-dictionare/main.js
+++ javascript-4-dictionare/main.js
@@ -1,1 +1,11 @@
-console.log("main js");
+const carteTelefoane = {
+    a: "0770000001",
+    b: "0770000002",
+    c: "0770000003",
+};
+
+console.log(carteTelefoane);
+
+const numarTelefonCautat = carteTelefoane.b;
+console.log(`Numarul de telefon a lui b este ${numarTelefonCautat}`);
+
