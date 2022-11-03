218f4f132221267f3e3951bc5838db3e42a1c432
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-03 19:08:25 +0200

    js-4 update: added search by string

--- javascript-4-dictionare/main.js
+++ javascript-4-dictionare/main.js
@@ -1,11 +1,13 @@
 const carteTelefoane = {
-    a: "0770000001",
-    b: "0770000002",
-    c: "0770000003",
+    cezarMocanu: "13241",
+    "cezar mocanu": "0770000001",
+    "vlad anghel": "0770000002",
+    "miruna maria": "0770000003",
 };
 
 console.log(carteTelefoane);
 
-const numarTelefonCautat = carteTelefoane.b;
-console.log(`Numarul de telefon a lui b este ${numarTelefonCautat}`);
+const numarTelefonCautat = carteTelefoane["cezar mocanu"];
+const numarTelefonCautat2 = carteTelefoane.cezarMocanu;
+console.log(`Numarul de telefon a lui cezar mocanu este ${numarTelefonCautat} sau ${numarTelefonCautat2}`);
 
