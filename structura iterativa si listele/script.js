62e514335530c11822c6c525a1f647cb33aa5b4e
GeorgiRadu <ciobanugeorgiana93@yahoo.com>
2022-11-04 16:22:03 +0200

    created exercise 2 and modified exercise 1

--- structura iterativa si listele/script.js
+++ structura iterativa si listele/script.js
@@ -3,6 +3,6 @@ console.log("Structura iterativa si listele");
 const MarcaList=["BMW", "Audi", "Dacia", "Audi", "Audi","Dacia", "Dacia", "Dacia", "BMW", "Volkswagen", "Opel", "Mercedes", "BMW", "Volkswagen"];
 const ModelList=["Seria 1", "A3", "Spring", "Q5", "A4", "Spring", "Docker", "Logan", "X5", "Golf", "Astra", "E200", "220i", "Golf"];
 
-for(let i = o; i < MarcaList.longth; i++){
+for(let i=0; i<MarcaList.length; i++){
     console.log(`${MarcaList[i]} ${ModelList[i]}`);
 }
