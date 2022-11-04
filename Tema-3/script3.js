d98b7dc4145783959f0a74e7a7231aaf26487cf7
biancaghertan <bianca.ghertan@yahoo.ro>
2022-11-04 15:37:51 +0200

    finished ex 3

--- Tema-3/script3.js
+++ Tema-3/script3.js
@@ -1,14 +1,16 @@
 console.log("Exercitiul 3");
 
 const cumpararePretList=[16000, 9000, 20300, 6000, 8000, 10000, 6000, 35500, 18900, 41000, 22500, 20500, 11590];
-const cumpararePretMax= cumpararePretList;
-const cumpararePretMin= cumpararePretList;
+let cumpararePretMax= cumpararePretList[0];
+let  cumpararePretMin= cumpararePretList[0];
 for (i=0; i < cumpararePretList.length; i++) {
-
-if (cumpararePretMax <= cumpararePretList.length[i])
- 
-  if(  cumpararePretMin >= cumpararePretList.length[i]);
+  if (cumpararePretList[i]> cumpararePretMax) {
+    cumpararePretMax=cumpararePretList[i];
+  
+  }
+  if (cumpararePretList[i]< cumpararePretMin) {
+    cumpararePretMin=cumpararePretList[i];
+  }
 }
-
-console.log(`Max este ${cumpararePretMax} si min este ${cumpararePretMin}`);
-
+ console.log( cumpararePretMax);
+ console.log(cumpararePretMin);
