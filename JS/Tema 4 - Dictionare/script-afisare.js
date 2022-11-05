6047ab4e31c6ea1feb85f34d8ebead95ad0ae052
AleBarboni <alexa_wera@yahoo.com>
2022-11-05 12:48:35 +0200

    finished tema 4 - cerinta 1

--- JS/Tema 4 - Dictionare/script-afisare.js
+++ JS/Tema 4 - Dictionare/script-afisare.js
@@ -1,3 +1,11 @@
 console.log(`Tema 4 - Dictionare`);
 console.log(`Cerinta 1`);
 // Afisati numele primelor 20 de articole din baza de date.
+
+let first20Clothes = 0;
+for (i = 0; i < BAZA_DE_DATE.length; i++) {
+    const first20Clothes = BAZA_DE_DATE[i].nume;
+    if(i < 20) {
+        console.log(first20Clothes);
+    }
+}
