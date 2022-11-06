63be2a0f9b409e9fdab876553b70ef9f86385d9a
Andrei <hdwallpaperthemes@gmail.com>
2022-11-06 18:40:22 +0200

    Finished first 4 exercises

---
+++ javascript-4/script3.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+for (let i = 0; i < BAZA_DE_DATE.length; i++) {
+    const articol = BAZA_DE_DATE[i];
+    // const nrDimensiuni = articol.dimensiuni.length;
+    const {dimensiuni} = articol;
+    const {culori} = articol;
+
+    if ((dimensiuni.length < 4) && 
+        (culori.includes("ROSU") || culori.includes("VERDE"))) {
+        count++;
+    }
+}
+
+console.log(`Sunt ${count} articole ce au cel mult 3 dimensiuni si au culoarea verde sau rosu`);
