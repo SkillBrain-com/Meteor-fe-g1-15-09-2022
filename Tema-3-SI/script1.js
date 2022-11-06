39694f8af7492c3712decdedbef88ba2628d1837
Groza Zita <adi_groza@yahoo.com>
2022-11-06 11:32:16 +0200

    made the requested changes to ex 3

--- Tema-3-SI/script1.js
+++ Tema-3-SI/script1.js
@@ -1,21 +1,27 @@
 console.log ("Punctul 3");
 
-//let minPosition = pretCumparareList.indexOf(6000);
-const indexes = pretCumparareList.map((val, idx) => { 
-    console.log(`Index of ${val} is ${idx}`);
-    return idx;
-});
+let indexMax = 0;
+let indexMin = 0;
+let maxPretCumparare = pretCumparareList[0];
+let minPretCumparare = pretCumparareList[0];
 
-pretCumparareList.sort(function(a,b){return a-b});
-let minCump = pretCumparareList[0];
-pretCumparareList.sort(function(a,b){return b-a});
-let maxCump = pretCumparareList[0];
+for (let i = 0; i < pretCumparareList.length; i++) {
+  if (pretCumparareList[i] > maxPretCumparare) {
+    maxPretCumparare = pretCumparareList[i];
+    indexMax = i;
+  }
 
-console.log ( `Pretul minim al unei masini este ${minCump}`);
-console.log ( `Pretul maxim al unei masini este ${maxCump}`);
+  if (pretCumparareList[i] < minPretCumparare) {
+    minPretCumparare = pretCumparareList[i];
+    indexMin = i;
+  }
+
+}
+
+console.log ( `Pretul minim al unei masini este ${minPretCumparare}`);
+console.log ( `Pretul maxim al unei masini este ${maxPretCumparare}`);
 
 ("Punctul 3 - a");
 
-//console.log (`6000 se gaseste la pozitia ` + minPosition);
-console.log (`Masina cu cel mai mic pret de ${minCump} este un ${marcaList[4]} ${modelList[4]} sau ${marcaList[7]} ${modelList[7]}`);
-console.log (`Masina cu cel mai mare pret de ${maxCump} este ${marcaList[10]} ${modelList[10]}`);
+console.log (`Masina cu cel mai mic pret de ${pretCumparareList[indexMin]} este ${marcaList[indexMin]} ${modelList[indexMin]}`);
+console.log (`Masina cu cel mai mare pret de ${pretCumparareList[indexMax]} este ${marcaList[indexMax]} ${modelList[indexMax]}`);
