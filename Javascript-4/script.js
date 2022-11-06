4ec05785579e15ab45933d485139aaffa6fbffae
AlinaBogdan <alinabogdan1990@gmail.com>
2022-11-06 16:11:42 +0200

    Exercise 1 is done

---
+++ Javascript-4/script.js
@@ -0,0 +1,7 @@
+console.log("Numele primelor 20 de articole de imbracaminte sunt:")
+
+for (let i=0; i<20; i++) {
+    const numeleArticolelor = BAZA_DE_DATE[i].nume;
+    console.log(numeleArticolelor)
+}
+
