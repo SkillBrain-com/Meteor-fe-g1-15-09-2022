4e25dc0c95daf03dda795f6a12afb23339ade30b
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-11-06 14:20:15 +0200

    created tema3 and a db.js

---
+++ Paul.tema3/scriptDB.js
@@ -0,0 +1,7 @@
+const carName = ["BMW","Audi","Dacia","Audi","Audi","Dacia","Dacia","Dacia","BMW","Volkswagen","Opel","Mercedes","BMW","Volkswagen"];
+const carModel = ["Seria1","A3","Spring","Q5","A4","Spring","Dokker","Logan","X5","Golf","Astra","E 200","220i","Golf"];
+const carIngoingPrice = [16000,9000,17000,20300,6000,8000,10000,6000,35500,18900,41000,22500,20500,11590];
+const carOutgoingPrice = [18000,12000,18000,27300,12000,14000,12000,9000,37000,19900,43000,27400,21500,13590];
+const carYear = [2018,2014,2018,2016,2017,2016,2018,2016,2021,2020,2022,2015,2018,2012];
+const carCC = [2000,2500,1800,2500,3000,2500,2000,1500,2000,1800,2200,3000,1200,1400];
+const carTransmision = ["Automata","Manuala","Automata","Automata","Automata","Manuala","Manuala","Manuala","Automata","Manuala","Automata","Automata","Manuala","Automata"];
