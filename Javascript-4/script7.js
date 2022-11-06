e67c0c72d34bbd77b006b8d52abf7b3bf2273b03
AlinaBogdan <alinabogdan1990@gmail.com>
2022-11-06 21:22:27 +0200

    Exercise 7 is done

---
+++ Javascript-4/script7.js
@@ -0,0 +1,12 @@
+let count2 = 0;
+
+for (let i=0; i<BAZA_DE_DATE.length; i++) {
+    const imbracaminte = BAZA_DE_DATE[i];
+    const {dimensiuni} = imbracaminte;
+
+    if (dimensiuni.includes("L") &&
+       dimensiuni.includes("XL")) {
+       count2++;
+    }
+}
+console.log(`${count2} articole sunt dispoibile in marimea L si XL`);
