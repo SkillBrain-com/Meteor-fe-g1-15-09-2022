4d332efbeff18aad7dfbdbe9cc703fc6d400b0b0
AlinaBogdan <alinabogdan1990@gmail.com>
2022-11-06 20:43:56 +0200

    Exercise 5 is done

---
+++ Javascript-4/script5.js
@@ -0,0 +1,20 @@
+let reducereMare;
+let pozitieArticolReducereMare = 0;
+for (let i=0; i<BAZA_DE_DATE.length; i++) {
+    const imbracaminte = BAZA_DE_DATE[i];
+    const {nume, pret, procentReducere} = imbracaminte;
+    const reducere = (procentReducere * pret)/100;
+    const pretRedus = pret - reducere;
+    
+    if (reducereMare === undefined) {
+        reducereMare = reducere;
+    }
+    else if (reducere > reducereMare) {
+        reducereMare = reducere;
+        pozitieArticolReducereMare = i;
+    }  
+}
+
+const reducerea = (BAZA_DE_DATE[pozitieArticolReducereMare].procentReducere * BAZA_DE_DATE[pozitieArticolReducereMare].pret)/100;
+const pretFinalRedus = BAZA_DE_DATE[pozitieArticolReducereMare].pret - reducerea;
+console.log(`${BAZA_DE_DATE[pozitieArticolReducereMare].nume} cu reducerea: ${reducerea} cu pretul redus: ${pretFinalRedus}`)
