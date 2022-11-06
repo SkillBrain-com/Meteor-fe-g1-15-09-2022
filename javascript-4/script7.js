9c0e658011a418c349cc9adc5b834fbcd6fde3b8
Andrei <hdwallpaperthemes@gmail.com>
2022-11-06 22:47:48 +0200

    forgot to save some changes on exercise 7

--- javascript-4/script7.js
+++ javascript-4/script7.js
@@ -1,11 +1,11 @@
-let count = 0;
+let count2 = 0;
 
 for (let i = 0; i < BAZA_DE_DATE.length; i++) {
     const articol = BAZA_DE_DATE[i];
     const {dimensiuni} = articol;
     if (dimensiuni.includes("L") && dimensiuni.includes("XL")) {
-        count++;
+        count2++;
     }
 }
 
-console.log(`sunt ${count} produse ce sunt disponibile in marimea L si XL`);
+console.log(`sunt ${count2} produse ce sunt disponibile in marimea L si XL`);
