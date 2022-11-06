cbea859170abfe2484961b93dcbddfecf5da460a
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2022-11-06 14:50:53 +0200

    modified script1

--- Paul.tema3/script1.js
+++ Paul.tema3/script1.js
@@ -1,6 +1,7 @@
 console.log("Cerinta 1");
 
-let carName = 0;
+const carName = ["BMW","Audi","Dacia","Audi","Audi","Dacia","Dacia","Dacia","BMW","Volkswagen","Opel","Mercedes","BMW","Volkswagen"];
+const carModel = ["Seria1","A3","Spring","Q5","A4","Spring","Dokker","Logan","X5","Golf","Astra","E 200","220i","Golf"];
 
 for(let i = 0; i < carName.length; i++) {
     console.log (` ${carName[i]}  ${carModel[i]}`);
