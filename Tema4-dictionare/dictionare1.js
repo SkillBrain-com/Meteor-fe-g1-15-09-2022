cb3ebdec69115da982f7021ba5eba3b7e4725513
ovidiufilip10 <ovidiufilip10@yahoo.com>
2022-11-06 13:55:37 +0200

    Exercitiul 2 + modificari la ex. 1.

--- Tema4-dictionare/dictionare1.js
+++ Tema4-dictionare/dictionare1.js
@@ -1,13 +1,5 @@
-const histogram = {};
 for(let i = 0; i <= 19; i++){
 const articol = BAZA_DE_DATE[i];
 const numeArticol = articol.nume;
-if(histogram[numeArticol]===undefined){
-    histogram[numeArticol]=1;
+console.log(numeArticol)
 }
-else{
-    histogram[numeArticol]++;
-}
-
-}
-console.log(histogram)
