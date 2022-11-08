75b52102a5a264f78ea3373688445a6d9793da7c
ovidiufilip10 <ovidiufilip10@yahoo.com>
2022-11-08 17:24:29 +0200

    Ex 4 in 2 rezolvari + Ex 5

---
+++ Tema4-dictionare/dictionare5.js
@@ -0,0 +1,13 @@
+let reducereMax, pretRedMax, numeArtRedMax;
+for(let i = 0; i < BAZA_DE_DATE.length; i++){
+    const articol = BAZA_DE_DATE[i];
+    const numeArticol = articol.nume;
+    const reducerePret = articol.procentReducere;
+    const pret = articol.pret;
+if(reducereMax === undefined || reducereMax < reducerePret  ){
+reducereMax = reducerePret;
+pretRedMax= pret-pret*reducerePret/100;
+numeArtRedMax=numeArticol;
+}
+}
+console.log(`Articoulul ${numeArtRedMax} are cea mai mare reducere, de ${reducereMax}%, pretul final fiind ${Math.round(pretRedMax)}`)
