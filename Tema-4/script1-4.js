4a6ddba191d9775b9b829acc7026133c0ea5a226
biancaghertan <bianca.ghertan@yahoo.ro>
2022-11-10 13:26:22 +0200

    Created script1-4

---
+++ Tema-4/script1-4.js
@@ -0,0 +1,6 @@
+console.log(" Exercitiul 1 tema 4");
+
+for( let i=0; i<20; i++){
+    const numeArticole=BAZA_DE_DATE[i].nume;
+    console.log(`Numele primelor 20 articole de imbracaminte: ${numeArticole}`);
+}
