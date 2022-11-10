017ef33acf01bbba2cc1794b89eebabf76a64107
biancaghertan <bianca.ghertan@yahoo.ro>
2022-11-10 13:54:06 +0200

    Created script2-4

--- Tema-4/script1-4.js
+++ Tema-4/script1-4.js
@@ -4,3 +4,4 @@ for( let i=0; i<20; i++){
     const numeArticole=BAZA_DE_DATE[i].nume;
     console.log(`Numele primelor 20 articole de imbracaminte: ${numeArticole}`);
 }
+console.log(numeArticole)
---
+++ Tema-4/script2-4.js
@@ -0,0 +1,16 @@
+console.log("Cerinta 2");
+ 
+const histogram= {};
+ 
+for (let i=0; i< BAZA_DE_DATE.length; i++) {
+    const imbracaminte= BAZA_DE_DATE[i];
+    const {brand}=imbracaminte;
+
+    if (histogram[brand]=== undefined) {
+        histogram[brand]=1;
+    }
+    else { 
+        histogram[brand]++;}
+
+}
+console.log(histogram);
