59d451d0904ae568b59b679e6b4d3bbc81300a0b
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-11 19:04:38 +0200

    js-5 created a sum function and called the function

---
+++ javascript-5-functii/main.js
@@ -0,0 +1,37 @@
+let a = 10;
+let b = 20;
+let rezultat = a + b;
+
+console.log(rezultat);
+
+
+//DECLARAREA FUNCTIEI 
+// - CUM SE CHEAMA FUNCTIA?
+// - CE PARAMETRII ARE FUNCTIA(DATE DE INTRARE)?
+// - CUM FUNCTIONEAZA?
+
+/**
+    @param a the first number to be summed
+    @param b the second number to be summed
+*/
+function sum(a, b) {
+    const rezultat = a + b;
+
+    console.log(rezultat);
+}
+
+/* APELAM FUNCTIA
+    - specificam functia care va fi apelata
+    - specificam ce valori vor lua parametrii
+    (argumente - valorile preluate de parametrii in momentul apelarii)
+*/
+sum(10, 20);
+sum(40, 20);
+
+const y = 50;
+const z = 100;
+
+sum(y, z);
+
+
+
