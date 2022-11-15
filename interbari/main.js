8812c0a57444ab4551d1044738b33e4e24383b8c
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-15 19:04:32 +0200

    js intrebari

---
+++ interbari/main.js
@@ -0,0 +1,32 @@
+/****************************************************************** */
+/** Intrebare: cum accesez proprietariile dintr-o lsita de obiecte*/
+/****************************************************************** */
+// const listaObiecte = [
+//     {
+//         id: 0,
+//         nume: "Cezar",
+//         departament: 'FE'
+//     },
+//     {
+//         id: 1,
+//         nume: "Alexandra"
+//     },
+//     {
+//         id: 2,
+//         nume: "Adriana",
+//         departament: 'FE'
+//     },
+//     {
+//         id: 3,
+//         nume: "Simona"
+//     },
+//     {
+//         id: 4,
+//         nume: "Gabi",
+//         departament: 'FE'
+//     }
+// ];
+
+// for(let i = 0;i < listaObiecte.length; i++) {
+//     console.log(listaObiecte[i].departament);
+// }
