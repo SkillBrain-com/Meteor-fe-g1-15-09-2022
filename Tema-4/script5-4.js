ff6d3bfb43e4e46c2394cf97dc3ae193c59bbd2f
biancaghertan <bianca.ghertan@yahoo.ro>
2022-11-16 13:29:42 +0200

    Script 5, tema 4 done

--- Tema-4/script5-4.js
+++ Tema-4/script5-4.js
@@ -1,5 +1,27 @@
  console.log(" Cerinta 5");
 
+
  let reducereMare=0;
+ let articolReducereMare=0;
  for (let i=0; i< BAZA_DE_DATE.length; i++){
     const imbracaminte= BAZA_DE_DATE[i];
+    const {nume, pret, procentReducere}=imbracaminte;
+    const discount=(procentReducere*pret)/100;
+    const pretRedus= pret-discount;
+    
+
+    if (reducereMare=== undefined){
+      reducereMare=discount;
+
+    }
+    else  ( discount > reducereMare) 
+      reducereMare=discount;
+          articolReducereMare=i; 
+
+    
+    const disCount= ( BAZA_DE_DATE[articolReducereMare].procentReducere *BAZA_DE_DATE[articolReducereMare].pret)/100;
+    const pretRedusFinal= BAZA_DE_DATE[articolReducereMare].pret - disCount;
+    
+        console.log(`${BAZA_DE_DATE[articolReducereMare].nume} are reducere de: ${Math.round(disCount)} cu pretul ${Math.round(pretRedusFinal)}`);
+   }
+
