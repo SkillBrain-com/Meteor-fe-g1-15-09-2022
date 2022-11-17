c3025f27bfd667d8b877a072b0f8eb4dfd449fb8
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-17 20:25:39 +0200

    created calculator

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,7 +1,24 @@
-const nameInput = document.querySelector("#nameInput");
-const targetHeading = document.querySelector("#targetHeading");
+const inputNumar1 = document.querySelector("#inputNumar1");
+const inputNumar2 = document.querySelector("#inputNumar2");
+const butonClaculeaza = document.querySelector("#butonClaculeaza");
+const textRezultat = document.querySelector("#textRezultat");
 
+function sum(a, b) {
+    return a + b;
+}
 
-nameInput.addEventListener("keyup", function(event){
-    targetHeading.innerText = `Salut ${event.target.value}!`;
+function getInputValue(input){
+    const inputValue = input.value;
+    const safeText = inputValue.replaceAll(",", ".");
+    const number = Number(safeText);
+    return number;
+}
+
+butonClaculeaza.addEventListener("click", function(){
+    const numar1 = getInputValue(inputNumar1);
+    const numar2 = getInputValue(inputNumar2);
+
+    const rezultat = sum(numar1, numar2);
+
+    textRezultat.innerHTML = rezultat.toFixed(2);
 });
