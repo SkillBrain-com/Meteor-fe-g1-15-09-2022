00a54f6eb84effc189f98f60398d740f9ebcffde
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-17 20:51:19 +0200

    js dom created safe inputs

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -2,23 +2,54 @@ const inputNumar1 = document.querySelector("#inputNumar1");
 const inputNumar2 = document.querySelector("#inputNumar2");
 const butonClaculeaza = document.querySelector("#butonClaculeaza");
 const textRezultat = document.querySelector("#textRezultat");
+const errorText = document.querySelector("#errorText");
 
 function sum(a, b) {
     return a + b;
 }
 
-function getInputValue(input){
+function getInputValue(input) {
     const inputValue = input.value;
+    
+    if (inputValue.length === 0) {
+        return {
+            error: "Valoarea introdusa nu poate fi goala!",
+            value: 0,
+        };
+    }
+    
     const safeText = inputValue.replaceAll(",", ".");
     const number = Number(safeText);
-    return number;
+    const isNumberNaN = isNaN(number);
+
+    if (isNumberNaN === true) {
+        return {
+            error: "Valorile introduse nu pot contine litere!",
+            value: 0,
+        };
+    }
+    
+    return {
+        error: null,
+        value: number,
+    };
 }
 
 butonClaculeaza.addEventListener("click", function(){
-    const numar1 = getInputValue(inputNumar1);
-    const numar2 = getInputValue(inputNumar2);
+    const input1Data = getInputValue(inputNumar1);
+    const input2Data = getInputValue(inputNumar2);
 
-    const rezultat = sum(numar1, numar2);
+    if (input1Data.error !== null) {
+        errorText.innerText = input1Data.error;
+    }
+    else if (input2Data.error !== null) {
+        errorText.innerText = input2Data.error;
+    }
+    else {
+        errorText.innerText = "";
+    }
 
-    textRezultat.innerHTML = rezultat.toFixed(2);
+    const rezultat = sum(input1Data.value, input2Data.value);
+
+    textRezultat.innerText = rezultat.toFixed(2);
 });
