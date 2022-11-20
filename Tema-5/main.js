edea17b1dcd9aebbb5321ddffb3dea454850e9a8
biancaghertan <bianca.ghertan@yahoo.ro>
2022-11-20 22:34:08 +0200

    Cerinta 2

--- Tema-5/main.js
+++ Tema-5/main.js
@@ -1,7 +1,46 @@
+console.log("Cerinta 1")
+
 function myMessage() {
     const nume = "Bianca";
-    message=`Bine ai venit, ${nume}`;
-  
-console.log(message);
+    message=`Bine ai venit, ${nume}!`;
+
+    
+  console.log(message);
+
 }
 myMessage()
+
+
+
+console.log("Cerinta 2");
+
+const nameList=[ "Bianca", "Andreea", "Daniel", "Mihai", "Larisa", "Razvan"];
+
+function nameAndInitial(nameList, letter) {
+    const returnName=[];
+    for( let i=0; i< nameList.length; i++){
+        let firstLetter=nameList[i].charAt(0)
+       if (firstLetter===letter) {
+        returnName.push(nameList[i]);
+        result=returnName ;}
+        
+        if (firstLetter !== letter){
+            returnName !==null;
+            result=returnName;
+       }
+       
+       else {
+        result=null; 
+       }
+       }
+   console.log(result);
+    }
+    nameAndInitial(nameList, "B");
+    nameAndInitial(nameList, "D");
+    nameAndInitial(nameList, "C");
+    
+
+
+
+
+
