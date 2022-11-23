d4bc68b181a66e82c7fa73caf3ca7b68a52b5722
AleBarboni <alexa_wera@yahoo.com>
2022-11-23 15:03:52 +0200

    tema 6, cu niste erori

--- JS/Tema 6 - DOM Manipulation/script-Bancomat.js
+++ JS/Tema 6 - DOM Manipulation/script-Bancomat.js
@@ -12,20 +12,54 @@ un criteriu nu este validat se va afisa un mesaj de eroare corespunzator:
     - valoarea introdusa nu trebuie sa contina litere
     - valoarea introdusa nu poate fi negativa
     - valoarea introdusa nu poate fi goala
-    - valoarea minima de retragere este de 5 lei */
+    - valoarea minima de retragere este de 5 lei.
+ Extra:
+- Asigura-te ca interfata este potrivita si pentru telefonul mobil si tablete(ecrane mai mici). 
+  Ghideaza-te dupa designul de mai jos
+- In momentul in care utilizatorul scrie o suma aceasta se va afisa in interiorul butonului. 
+  In caz ca valoarea este invalida se butonul va avea culoarea gri, indicand faptul ca este dezactivat. 
+  De asemenea se va afisa si mesajul de eroare sub input.*/
 
 let sumaDisponibila = document.querySelector(".suma_disponibila");
-sumaDisponibila.innerText = Math.ceil(Math.random() * 1000);
-
-let retragere = document.querySelector(".retragere").value;
+let sold = sumaDisponibila.value;
+let valoare = document.querySelector(".valoare");
+let retragere = document.querySelector(".retragere");
+let errorText = document.querySelector(".errorText");
+sumaDisponibila.innerText = 500;
+//Math.ceil(Math.random() * 1000);
 
 let sumaRamasa = 0;
+let soldNou = 0;
 function retragereBani(sold, sumaRetragere) {
-  if (sumaRetragere < sold) {
+  sold = sumaDisponibila.innerText;
+  if (sumaRetragere == " ") {
+    errorText.innerText = "campul nu poate fi gol";
+  } else if (sumaRetragere < 5 && sumaRetragere != " ") {
+    errorText.innerText = "suma retrasa nu poate fi mai mica de 5 lei";
+  } else if (sumaRetragere > sold) {
+    errorText.innerText =
+      "suma retrasa nu trebuie sa depaseasca suma disponibila";
+  } else if (sumaRetragere < sold && sumaRetragere > 4) {
     sumaRamasa = sold - sumaRetragere;
-    console.log(sumaRamasa);
-  } else {
-    Error.innerText = "Suma introdusa nu poate depasi suma din cont";
+    //console.log(sumaRamasa);
+    soldNou = sumaRamasa;
+    sold = soldNou;
+    sumaDisponibila.innerText = sold;
+    document.querySelector(".valoare").value = null;
   }
 }
-retragereBani(sumaDisponibila, retragere);
+retragereBani(sumaDisponibila, valoare);
+
+retragere.addEventListener("click", function () {
+  let valoareIntrodusa = Number(valoare.value);
+  const rezultat = retragereBani(sumaDisponibila, valoareIntrodusa);
+});
+
+valoare.addEventListener("keyup", () => {
+  if (
+    (valoare.value.length =
+      0 || valoare.value < 5 || valoare.value > sumaDisponibila.value)
+  )
+    retragere.disabled = true;
+  else retragere.disabled = false;
+});
