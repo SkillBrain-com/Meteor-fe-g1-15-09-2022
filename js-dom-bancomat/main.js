707af6f3dbceb33a7f65eef9cc9ae57750faeb3a
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-24 20:11:30 +0200

    creted getValueFromInput

--- js-dom-bancomat/main.js
+++ js-dom-bancomat/main.js
@@ -4,16 +4,22 @@ const inputSumaRetragere = document.querySelector("#inputSumaRetragere");
 const textEroare = document.querySelector("#textEroare");
 const butonRetragere = document.querySelector("#butonRetragere");
 
+function getValueFromInput(inputElement) {
+    const text = inputElement.value;
+    const parsedNumber = Number(text);
+
+    return parsedNumber;
+}
+
 /** MODEL */
-let sumaDisponibila = 500;
-let eroare = null;
+let sumaDisponibila;
+let eroare;
+function setModel(suma, textEroare) {
+    sumaDisponibila = suma;
+    eroare = textEroare;
+}
 
-/** VIEW */
-
-/** VIEW - EVENT LISTENERS */
-
-/** CONTROLLER */
-
+/**BUSINESS LOGIC */
 function verificaSumaRetragere(sumaRetrasa, sold) {
     if (sumaRetrasa > sold) {
         return "Suma retrasa nu poate fi mai mare ca soldul";
