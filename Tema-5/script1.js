837dbdef798420bb937d42a9a57cd16a30a0695c
Groza Zita <adi_groza@yahoo.com>
2022-11-28 11:42:17 +0200

    created Tema 5 - Ex 2

---
+++ Tema-5/script1.js
@@ -0,0 +1,10 @@
+console.log(`Tema 5 - Ex 1`)
+// Sa se construiasca o functie care va afisa un mesaj de salutare pentru userul logat intr-o aplicatie.
+// Functia va primi ca parametru numele utilizatorului si va afisa mesajul "Bine ai venit, ${nume}!"
+
+
+function loggedInUser (name){
+    console.log(`Bine ai venit, ${name}!`);    
+}
+
+loggedInUser("Adriana");
