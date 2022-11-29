9c3db0c954a2ff02e315c75fe1d8b41739b48f60
cezarmocanu <cezarmocanu2014@gmail.com>
2022-11-29 19:56:44 +0200

    array map

--- advanced-js/main.js
+++ advanced-js/main.js
@@ -47,15 +47,16 @@ const f2 = () => {};
 //ES6
 // const square = (nr) => nr * nr;
 
+/* HIGH ORDER FUNCTIONS */
 const sum = (a, b) => a + b;
 const times = (a, b) => a * b;
 const doOperation = (operand1, operand2, operation) => operation(operand1, operand2);
 
 const sumOfNumbers = doOperation(10,10, sum);
 const productOfNumbers = doOperation(10,10, times);
-console.log(sumOfNumbers);
-console.log(productOfNumbers);
-console.log(doOperation(10, 10, (a, b) => a * 2 + b * 4));
+// console.log(sumOfNumbers);
+// console.log(productOfNumbers);
+// console.log(doOperation(10, 10, (a, b) => a * 2 + b * 4));
 
 
 const isEven = (value) => value % 2 === 0
@@ -64,5 +65,51 @@ const myFilter = (value) => value.startsWith("A") && value.includes("@");
 /*filterFunction(value: any) => boolean */
 const checkFilter = (value, filterFunction) => filterFunction(value);
 
-console.log(checkFilter(10, isEven));
-console.log(checkFilter("AhelloWorld@", myFilter));
+// console.log(checkFilter(10, isEven));
+// console.log(checkFilter("AhelloWorld@", myFilter));
+
+
+/* MAP - transforma fiecare element intr-o valoare noua*/
+
+function creeazaListaPatrate(listaNumere) {
+    const listaNoua = [];
+    for(let i = 0;i < listaNumere.length; i++) {
+        const patrat = listaNumere[i] * listaNumere[i];
+        listaNoua.push(patrat);
+    }
+
+    return listaNoua;
+}
+
+function exempluMap(){
+    const listaNumere = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const listaNoua = creeazaListaPatrate(listaNumere);
+    
+    //map creeaza intern o lista noua si o returneaza
+    const listaNouaMap = listaNumere.map((x) => x * x); 
+
+    console.log(listaNoua);
+    console.log(listaNouaMap);
+}
+
+exempluMap();
+
+
+
+/* FOREACH */
+
+/* FILTER */
+
+/* EVERY */
+
+/* SOME/ANY */
+
+/* REDUCE */
+
+
+
+
+
+
+
+
