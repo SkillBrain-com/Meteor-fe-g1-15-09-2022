a1b5d8892c931cc656a5c1e6ad4d16f69ab60104
Livia Soare <livia.soare07@yahoo.com>
2022-11-30 00:41:01 +0200

    Created list

---
+++ Tema-3-Liste/script.js
@@ -0,0 +1,7 @@
+console.log("Exercitiul-1");
+const Marca = ['BMW','Audi','Dacia','Audi','Audi','Dacia','Dacia','Dacia','BMW','Volkswagen','Opel','Merceds','BMW','Volkswagen'];
+const Model = ['Seria 1','A3','Spring','Q5','A4','Spring','Docker','Logan','X5','Golf','Astra','E 200','220i','Golf'];
+let i=0;
+for(i=0; i < Marca.length; i++){
+    console.log(`${Marca[i]} ${Model[i]}\n`);
+}
