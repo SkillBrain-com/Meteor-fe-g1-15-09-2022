7e2340454b17c66b947ed30b7b7ac0cb36b70110
Livia Soare <livia.soare07@yahoo.com>
2022-11-30 00:43:25 +0200

    Completed task 2

--- Tema-3-Liste/script.js
+++ Tema-3-Liste/script.js
@@ -5,3 +5,13 @@ let i=0;
 for(i=0; i < Marca.length; i++){
     console.log(`${Marca[i]} ${Model[i]}\n`);
 }
+
+console.log("Exercitiul-2");
+
+const PretCumparare = [16000,9000,17000,20300,6000,8000,10000,6000,35500,18900,41000,22500,20500,11590];
+let MediePret=0;
+for(i=0; i < PretCumparare.length; i++){
+   MediePret=MediePret+PretCumparare[i]; 
+}
+MediePret=MediePret/PretCumparare.length;
+console.log(`Media preturilor de cumparare este ${MediePret.toFixed(2)}`);
