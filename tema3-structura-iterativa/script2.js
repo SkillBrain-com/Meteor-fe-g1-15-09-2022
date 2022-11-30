7f6c4eb1268b0ebc6b91f0b759036a183688622b
Narcisa Mihaela Hij <hijnarcisa@yahoo.com>
2022-11-30 19:02:54 +0200

    tema3 ex2

---
+++ tema3-structura-iterativa/script2.js
@@ -0,0 +1,12 @@
+
+console.log(`exercitiul3-2`);
+
+const pretCumparare = [16000, 9000, 17000, 20300, 6000, 8000, 10000, 6000, 35500, 18900, 41000, 22500, 20500, 11590];
+let suma = 0;
+
+for (let i = 0; i < pretCumparare.length; i++) {
+    suma += pretCumparare[i];
+}
+
+const mediaPretCumparare = suma / pretCumparare.length;
+console.log(`Pretul mediu de cumparare este ${Math.round(mediaPretCumparare)}, mai exact ${mediaPretCumparare}`);
