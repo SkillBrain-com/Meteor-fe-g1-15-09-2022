44c6112080989d9c7d4d1281e36802a1d0aaa1eb
Narcisa Mihaela Hij <hijnarcisa@yahoo.com>
2022-12-07 16:34:50 +0200

    exercitiul3c)

---
+++ tema5-functii/main.js
@@ -0,0 +1,13 @@
+function main() {
+    const v1 = 123;
+    const v2 = "W@rl is not my passowrd";
+  
+    // myLibraryFunction("null", "undefined"); 
+    // welcomeMessage("Serban Andrei");
+    // const firstLetter = findFirstLetter(["Ana" , "Maria", "Luci", "Nicu", "Antonia", "Gica", "Cristina"], "A");
+    // console.log(firstLetter);
+    console.log(selecteazaDupaCantitate(PRODUSE, 49));
+    console.log(selecteazaPreturile(PRODUSE));
+    console.log(calculeazaValoareMedie(PRODUSE));
+  }
+  main();
