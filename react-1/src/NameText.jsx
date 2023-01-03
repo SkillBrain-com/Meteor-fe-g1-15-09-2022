c582450f7a7163b29be00e80678e7694929a2f9f
cezarmocanu <cezarmocanu2014@gmail.com>
2023-01-03 19:17:59 +0200

    created sample components

---
+++ react-1/src/NameText.jsx
@@ -0,0 +1,14 @@
+import MyInput from "./MyInput";
+
+function NameText(){
+    return (
+        <div>
+            <MyInput/>
+            <h1>Hello Cezar</h1>
+            <h2>Hello Cezar</h2>
+            <h3>Hello Cezar</h3>
+        </div>
+    )
+}
+
+export default NameText;
