9545d6e91921a47a8e2f8d9a17e42674dcb8beaf
cezarmocanu <cezarmocanu2014@gmail.com>
2023-01-03 21:33:18 +0200

    Added variable app state

--- react-1/src/NameText.jsx
+++ react-1/src/NameText.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import { getElementColorFromClicks } from './utils';
 
 //properties -> props
-function NameText(props){
+function NameText(props) {
     const [clicks, setClicks] = useState(0);
+    const [message, setMessage] = useState("salut");
+
+    function handleOnInputChange(event) {
+        const newMessage = event.target.value;
+        setMessage(newMessage);
+    };
 
     const handleOnClick = () => {
         const newClicks = clicks + 1;
@@ -10,13 +17,18 @@ function NameText(props){
     };
 
     return (
-        <div onClick={handleOnClick}>
-            <h1>Hello {props.name}</h1>
-            <h2>Hello {props.name}</h2>
-            <h3>Hello {props.name}</h3>
+        <div
+            className={getElementColorFromClicks(clicks)}
+            onClick={handleOnClick}
+        >
+            <h1>Hello {props.name} {message}</h1>
+            <h2>Hello {props.name} {message}</h2>
+            <h3>Hello {props.name} {message}</h3>
             <h1>{clicks}</h1>
+            <input type="text" onChange={handleOnInputChange} />
         </div>
     )
+
 }
 
 export default NameText;
