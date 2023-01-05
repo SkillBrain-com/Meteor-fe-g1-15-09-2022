850ca69af9426be3ffe2cebc6623f5094b6e3f71
cezarmocanu <cezarmocanu2014@gmail.com>
2023-01-05 21:04:53 +0200

    added controls in children

--- react-2/src/App.jsx
+++ react-2/src/App.jsx
@@ -4,25 +4,23 @@ import Termometru from './Termometru';
 function App() {
   const [temperatura, setTemperatura] = useState(1);
 
-  function handleIncrementClick() {
-    setTemperatura(temperatura + 1);
-  }
-
-  function handleDecrementClick() {
-    setTemperatura(temperatura - 1);
-  }
-
   return (
     <div>
-      <Termometru unit="C" temperatura={temperatura}/>
-      <Termometru unit="F" temperatura={temperatura}/>
-      <Termometru unit="K" temperatura={temperatura}/>
-      <button onClick={handleDecrementClick}>
-        <h1>-</h1>
-      </button>
-      <button onClick={handleIncrementClick}>
-        <h1>+</h1>
-      </button>
+      <Termometru
+        unit="C" 
+        temperatura={temperatura}
+        setTemperatura={setTemperatura}
+      />
+      <Termometru
+        unit="F" 
+        temperatura={temperatura}
+        setTemperatura={setTemperatura}
+      />
+      <Termometru
+        unit="K" 
+        temperatura={temperatura}
+        setTemperatura={setTemperatura}
+      />
     </div>
   )
 }
--- react-2/src/Termometru.jsx
+++ react-2/src/Termometru.jsx
@@ -8,19 +8,46 @@ function toFahrenheit(celsius) {
 }
 
 function Termometru(props) {
-    
+    function handleIncrementClick() {
+        if(props.unit === 'C'){
+            props.setTemperatura(props.temperatura + 1);
+            return;
+        }
+
+        if (props.unit === 'F') {
+            const unitValue = .5556;
+            props.setTemperatura(props.temperatura + unitValue);
+            return;
+        }
+
+        props.setTemperatura(props.temperatura + 1);
+    }
+
+    function handleDecrementClick() {
+        if(props.unit === 'C'){
+            props.setTemperatura(props.temperatura - 1);
+            return;
+        }
+
+        if (props.unit === 'F') {
+            props.setTemperatura(props.temperatura - 1.8);
+            return;
+        }
+
+        props.setTemperatura(props.temperatura - 1);
+    }
 
     return (
         <div>
             {props.unit === 'C' && <h1>{props.temperatura.toFixed(2)} {props.unit}</h1>}
             {props.unit === 'K' && <h1>{toKelvin(props.temperatura).toFixed(2)} {props.unit}</h1>}
             {props.unit === 'F' && <h1>{toFahrenheit(props.temperatura).toFixed(2)} {props.unit}</h1>}
-            {/* <button onClick={handleDecrementClick}>
+            <button onClick={handleDecrementClick}>
                 <h1>-</h1>
             </button>
             <button onClick={handleIncrementClick}>
                 <h1>+</h1>
-            </button> */}
+            </button>
         </div>
     );
 }
