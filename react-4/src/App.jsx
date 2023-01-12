0e1ad5671787a159f57b51f1a3b9e4ed016becf9
cezarmocanu <cezarmocanu2014@gmail.com>
2023-01-12 21:08:10 +0200

    created modals

---
+++ react-4/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import ProfileModal from "./components/ProfileModal";
+import PrimaryButton from "./shared/PrimaryButton";
+import OffersModal from "./components/OffersModal";
+
+function App() {
+  const [showProfileModal, setShowProfileModal] = useState(false);
+  const [showOffersModal, setShowOffersModal] = useState(false);
+
+  const profile = {
+    name: "Cezar Mocanu",
+    phone: "49236853",
+    email: "cedfsdfs@yahoo.com",
+    address: "Brasov Brasov Calea Molidului 19",
+  };
+
+  function handleProfileModalOpen() {
+    setShowProfileModal(true);
+  }
+
+  function handleProfileModalClose() {
+    setShowProfileModal(false);
+  }
+
+  function handleOfferModalOpen() {
+    setShowOffersModal(true);
+  }
+
+  function handleOfferModalClose() {
+    setShowOffersModal(false);
+  }
+
+  return (
+    <div>
+      <div className="flex p-2 drop-">
+        <div className="grow">
+          <h1>METEOR</h1>
+        </div>
+        <div className="flex gap-2">
+          <PrimaryButton
+            label={"Profilul meu"}
+            handleOnClick={handleProfileModalOpen}
+          />
+          <PrimaryButton
+            label={"Oferte"}
+            handleOnClick={handleOfferModalOpen}
+          />
+        </div>
+        
+      </div>
+      {showProfileModal && (
+        <ProfileModal
+          profile={profile}
+          handleOnClose={handleProfileModalClose}
+        />
+      )}
+      {
+        showOffersModal && (
+          <OffersModal
+            handleOnClose={handleOfferModalClose}
+          />
+        )
+      }
+    </div>
+  )
+}
+
+export default App
