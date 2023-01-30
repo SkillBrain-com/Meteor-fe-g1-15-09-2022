52f021d2a6c6847d370e42adeadd435afdeee3fc
cezarmocanu <cezarmocanu2014@gmail.com>
2023-01-30 21:19:16 +0200

    added list

--- react-7/src/App.jsx
+++ react-7/src/App.jsx
@@ -1,13 +1,52 @@
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import { ThemeProvider } from './ThemeContext';
 import Button from './Button';
 import Wrapper from './Wrapper';
 
+const Data = [
+  {letter: "A"},
+  {letter: "B"},
+  {letter: "C"},
+  {letter: "D"},
+  {letter: "E"},
+];
+
 function App() {
   const [theme, setTheme] = useState("dark");
+  const [letterList, setLetterList] = useState([]);
+  const [letterInputValue, setLetterInputValue] = useState("");
+
+  useEffect(() => {
+    setLetterList([...Data]);
+  }, []);
+
+  function handleAddElementClick(){
+    if (letterInputValue.length < 3){
+      return;
+    }
+    const newLetterList = [{letter: letterInputValue}, ...letterList];
+    setLetterList(newLetterList);
+    setLetterInputValue("");
+  }
+
+  function handleLetterInputChange(e){
+    setLetterInputValue(e.target.value);
+  }
 
   return (
     <div className="App">
+      <input
+        value={letterInputValue}
+        onChange={handleLetterInputChange}
+      />
+      <button
+        className="btn"
+        onClick={handleAddElementClick}
+      >Add Element</button>
+      {
+        letterList.map((el) => <h1>Letter {el.letter}</h1>)
+      }
+
       <ThemeProvider value={{theme, setTheme}}>
         <Wrapper>
           <Wrapper>
